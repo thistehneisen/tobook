be6fd26037109a9fe68e2e21eb6c14f55d0565f2
Hieu Nguyen <hieuh25@gmail.com>
2014-07-18 00:10:22 +0300

    Simple fabfile for deployment

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import cd, run, task, hosts, env
+import os
+
+HOME = os.getenv('HOME')
+
+
+def _deploy():
+    env.user = 'root'
+    with cd('/var/www'):
+        # pull latest source
+        run('git pull')
+
+
+@task(alias='rc')
+@hosts('varaa.co')
+def deploy_stag():
+    _deploy()
+
+
+@task(alias='rc')
+@hosts('varaa.co')
+def run_command(command='ls'):
+    '''
+    Run any command in server
+    '''
+    env.user = 'root'
+    with cd('/var/www'):
+        run(command)
