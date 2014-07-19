03828ec7eb40b62bd3a7240f8d40ce1e2990e5fc
kcjpop <kcj.pop.com@gmail.com>
2014-07-19 16:03:27 +0300

    Hack to bypass payment.

--- marketing/async-getPaymentStatus.php
+++ marketing/async-getPaymentStatus.php
@@ -1,28 +1,28 @@
 <?php
-	require_once("common/DB_Connection.php");	
+    require_once("common/DB_Connection.php");
     require_once("common/functions.php");
 
     $result = "success";
     $error = "";
     $data = array();
-    
+
     $userId = mysql_escape_string($_POST['userId']);
-    $planGroupCode = mysql_escape_string($_POST['planGroupCode']);    
-    
+    $planGroupCode = mysql_escape_string($_POST['planGroupCode']);
+
     $sql = "select if(expired_time > now(), 1, 0) as status, account_code from tbl_owner_premium where plan_group_code = '$planGroupCode' and owner = '$userId'";
     $dataResult = $db->queryArray( $sql );
     if( $dataResult == null ){
-    	$status = 0;
-    	$accountCode = "";
+        $status = 0;
+        $accountCode = "";
     }else{
-    	$status = $dataResult[0]['status'];
-    	$accountCode = $dataResult[0]['account_code'];
+        $status = $dataResult[0]['status'];
+        $accountCode = $dataResult[0]['account_code'];
     }
-    
+
     $data['accountCode'] = $accountCode;
-    $data['status'] = $status;
+    $data['status'] = 1; // $status;
     $data['result'] = $result;
     $data['error'] = $error;
     header('Content-Type: application/json');
-    echo json_encode($data);    
+    echo json_encode($data);
 ?>
