2b6f8a17b67da5535b587480da69116927a58731
kcjpop <kcj.pop.com@gmail.com>
2014-07-22 22:59:06 +0300

    Migrate users and relationships with groups.

--- cmd/Migrate/Cashier.php
+++ cmd/Migrate/Cashier.php
@@ -5,7 +5,14 @@ class Cashier extends Base {
 
 	protected $schemaManager;
 	protected $currentUser;
-	protected $prefix;
+	protected $username;
+
+	/**
+	 * Contain map from old IDs to new IDs
+	 *
+	 * @var array
+	 */
+	protected $map = [];
 
 	public function __construct($output, $db)
 	{
@@ -23,8 +30,12 @@ class Cashier extends Base {
 		// $usernames = $this->getUsernames();
 		$usernames = ['jenistar'];
 		foreach ($usernames as $username) {
-			$this->prefix = $username;
+			$this->username = $username;
+
+			$this->info("Proccessing data of [{$username}]");
 			$this->migrateGroups();
+			$this->migrateUsers();
+			$this->migrateUserGroup();
 		}
 	}
 
@@ -50,18 +61,18 @@ class Cashier extends Base {
 	public function getCurrentUser()
 	{
 		if ($this->currentUser === false
-		 || $this->currentUser['vuser_login'] !== $this->prefix) {
+		 || $this->currentUser['vuser_login'] !== $this->username) {
 			// Get user information from tbl_user_mast
 			$stm = $this->queryBuilder()
 				->select('*')
 				->from('tbl_user_mast', 'u')
 				->where('u.vuser_login = ?')
-				->setParameter(0, $this->prefix)
+				->setParameter(0, $this->username)
 				->execute();
 
 			$user = $stm->fetch();
 			if ($user === false) {
-				$this->error("Cannot find information of $this->prefix. Retry later!");
+				$this->error("Cannot find information of $this->username. Retry later!");
 			}
 
 			$this->currentUser = $user;
@@ -80,11 +91,11 @@ class Cashier extends Base {
 		// Array to map old group IDs to new group IDs
 		$map = [];
 
-		$table = $this->prefix.'_sma_groups';
+		$this->text('Migrating groups...');
 		// Get all existing groups
 		$stm = $this->queryBuilder()
 			->select('*')
-			->from($table, 'g')
+			->from($this->username.'_sma_groups', 'g')
 			->execute();
 		
 		while($row = $stm->fetch()) {
@@ -97,7 +108,71 @@ class Cashier extends Base {
 			$map[$oldId] = $this->db->lastInsertId();
 		}
 
+		$this->map['groups'] = $map;
 		return $map;
+	}
+
+	public function migrateUsers()
+	{
+		$user = $this->getCurrentUser();
+		if ($user === false) {
+			return;
+		}
+
+		$this->text("Migrating users...");
+		$map = [];
+
+		// Get all sub-users
+		$stm = $this->queryBuilder()
+			->select('*')
+			->from($this->username.'_sma_users', 'u')
+			->execute();
+
+		while ($row = $stm->fetch()) {
+			$oldId = $row['id'];
+
+			unset($row['id']);
+			$row['owner_id'] = $user['nuser_id'];
+
+			// Move from old table to correct one
+			$this->db->insert('sma_users', $row);
+
+			$map[$oldId] = $this->db->lastInsertId();
+		}
+
+		$this->map['users'] = $map;
+		return $map;
+	}
+
+	public function migrateUserGroup()
+	{
+		$this->text('Migrating users and groups relationship...');
+
+		$user = $this->getCurrentUser();
+		if ($user === false || 
+			empty($this->map['groups']) || 
+			empty($this->map['users'])) {
+			$this->error('ERROR: Either Group or User map is empty.');
+			return;
+		}
+
+		$stm = $this->queryBuilder()
+			->select('*')
+			->from($this->username.'_sma_users_groups', 'ug')
+			->execute();
+
+		while ($row = $stm->fetch()) {
+			unset ($row['id']);
+
+			$userId  = $row['user_id'];
+			$groupId = $row['group_id'];
+
+			$row['user_id']  = $this->map['users'][$userId];
+			$row['group_id'] = $this->map['groups'][$groupId];
+			$row['owner_id'] = $user['nuser_id'];
+
+			$this->db->insert('sma_users_groups', $row);
+		}
 	}
 
 }
