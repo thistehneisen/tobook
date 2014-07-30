34c91917b52bffd93b114a18ad3ad3e2631814f4
Hieu Nguyen <hieuh25@gmail.com>
2014-07-30 07:15:50 +0300

    Ref #24: use swiftmailer and mandrill to send mail after signup

--- includes/configsettings.php
+++ includes/configsettings.php
@@ -18,6 +18,11 @@ define('MYSQL_DB',          $config['db']['name']);
 define('MYSQL_TABLE_PREFIX','tbl_');
 define('BASE_URL_PART','');
 
+define('SMTP_HOST', 'smtp.mandrillapp.com');
+define('SMTP_PORT', 587);
+define('SMTP_USERNAME', 'hieu@varaa.com');
+define('SMTP_PASSWORD', isset($config['smtp']['password']) ? $config['smtp']['password'] : 'CTzo04IR5HYpn9LjCGV-7A');
+
 
 $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
 $protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) .$s . "://";
@@ -34,41 +39,37 @@ define('BASE_URL',$baseUrl);
 
 $_SESSION["session_template_dir"] = 'templates';
 
-/****************** Editor settings starts ***************/
 define('EDITOR_USER_IMAGES', 'uploads/siteimages/' );
-
-/**************** Editor settings ends ********************/
-
 define('USER_SITE_UPLOAD_PATH','workarea/sites/');
 define('EDITOR_IMAGES','editor/images/');
 define('EDITOR_APP_SOCIAL_SHARE',2);
 define('TEMPLATE_PREFIX','T');
 
 // Manage Currency
-$currencyArray = array( "USD" => array('title'=>'US Dollar','code' => 'USD','symbol' => '$'),
-        "EUR" => array('title'=>'Euro','code' => 'EUR','symbol' => '&euro;'),
-        "GBP" => array('title'=>'Pound','code' => 'GBP','symbol' => '&pound;'),
-        "CAD" => array('title'=>'Canadian Dollar','code' => 'CAD','symbol' => '$'),
-        "INR" => array('title'=>'Indian Rupee','code' => 'INR','symbol' => '&#x20a8;')
+$currencyArray = array( 
+    "USD" => array('title'=>'US Dollar','code' => 'USD','symbol' => '$'),
+    "EUR" => array('title'=>'Euro','code' => 'EUR','symbol' => '&euro;'),
+    "GBP" => array('title'=>'Pound','code' => 'GBP','symbol' => '&pound;'),
+    "CAD" => array('title'=>'Canadian Dollar','code' => 'CAD','symbol' => '$'),
+    "INR" => array('title'=>'Indian Rupee','code' => 'INR','symbol' => '&#x20a8;')
 );
 
-$fontList = array(	'Arial' 				=> 'Arial, Helvetica, sans-serif',
-                        'Arial Black' 			=> '"Arial Black", Gadget, sans-serif',
-                        'Comic Sans MS' 		=> 'Comic Sans MS, Comic Sans MS, cursive',
-                        'Courier' 				=> '"Courier New", Courier, monospace',
-                        'Courier New' 			=> '"Courier New", Courier, monospace',
-                        'Georgia' 				=> 'Georgia, "Times New Roman", Times, serif',
-                        'Geneva' 				=> 'Geneva, Arial, Helvetica, sans-serif',
-                        'Impact' 				=> 'Impact, Impact, Charcoal, sans-serif',
-                        'Lucida Console' 		=> 'Lucida Console, Monaco, monospace',
-                        'Lucida Sans Unicode' 	=> 'Lucida Sans Unicode, Lucida Grande, sans-serif',
-                        'MS Sans Serif' 		=> 'MS Sans Serif, Geneva, sans-serif',
-                        'MS Serif' 				=> 'MS Serif, New York, serif',
-                        'Palatino Linotype' 	=> 'Palatino Linotype, Book Antiqua, Palatino, serif',
-                        'Tahoma' 				=> 'Tahoma, Geneva, sans-serif',
-                        'Times New Roman' 		=> '"Times New Roman", Times, serif',
-                        'Trebuchet MS' 			=> 'Trebuchet MS, Trebuchet MS, sans-serif',
-                        'Verdana' 				=> 'Verdana, Arial, Helvetica, sans-serif'
-                        );
-
-?>
+$fontList = array(	
+    'Arial' 				=> 'Arial, Helvetica, sans-serif',
+    'Arial Black' 			=> '"Arial Black", Gadget, sans-serif',
+    'Comic Sans MS' 		=> 'Comic Sans MS, Comic Sans MS, cursive',
+    'Courier' 				=> '"Courier New", Courier, monospace',
+    'Courier New' 			=> '"Courier New", Courier, monospace',
+    'Georgia' 				=> 'Georgia, "Times New Roman", Times, serif',
+    'Geneva' 				=> 'Geneva, Arial, Helvetica, sans-serif',
+    'Impact' 				=> 'Impact, Impact, Charcoal, sans-serif',
+    'Lucida Console' 		=> 'Lucida Console, Monaco, monospace',
+    'Lucida Sans Unicode' 	=> 'Lucida Sans Unicode, Lucida Grande, sans-serif',
+    'MS Sans Serif' 		=> 'MS Sans Serif, Geneva, sans-serif',
+    'MS Serif' 				=> 'MS Serif, New York, serif',
+    'Palatino Linotype' 	=> 'Palatino Linotype, Book Antiqua, Palatino, serif',
+    'Tahoma' 				=> 'Tahoma, Geneva, sans-serif',
+    'Times New Roman' 		=> '"Times New Roman", Times, serif',
+    'Trebuchet MS' 			=> 'Trebuchet MS, Trebuchet MS, sans-serif',
+    'Verdana' 				=> 'Verdana, Arial, Helvetica, sans-serif'
+);
