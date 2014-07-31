4c49a02579ae3747a7c3f33aecda215bb1bcf6fb
Quang Hung <hungnq1989@gmail.com>
2014-07-31 21:55:23 +0300

    issues #32: fix form create employee and create service (repeating fields)

--- appointment/library/app/controllers/pjAdminEmployees.controller.php
+++ appointment/library/app/controllers/pjAdminEmployees.controller.php
@@ -385,7 +385,7 @@ class pjAdminEmployees extends pjAdmin
 				$pjEmployeeModel = pjEmployeeModel::factory()
 					->select('t1.*, t2.*')
 					->join('pjMultiLang', "t2.model='pjEmployee' AND t2.foreign_id=t1.id AND t2.field='name' AND t2.locale='".$this->getLocaleId()."'", 'left outer') 
-					->where('t1.owner_id', $owner_id)
+					->where('t1.owner_id', $owner_id);
                     // Éo hiểu dòng này làm cái mẹ gì, nên thôi comment cho lành
                     // ->where('t1.calendar_id', $this->getForeignId());
 				
