0143014fca7798c6ca07c9b4a2090b04d2f1c96d
kcjpop <kcj.pop.com@gmail.com>
2014-08-05 08:09:54 +0300

    Add assets.

--- laravel/app/views/layouts/default.blade.php
+++ laravel/app/views/layouts/default.blade.php
@@ -6,12 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>
         @section('title')
-        Varaa
+            Varaa
         @show
     </title>
     
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     @yield('styles')
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
