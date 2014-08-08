82e5acaee6f16172e0ee09e3dd3248123f6f65ee
An Cao <kcj.pop.com@gmail.com>
2014-08-08 14:17:23 +0300

    Define local hostname.

--- laravel/app/config/database.php
+++ laravel/app/config/database.php
@@ -55,12 +55,12 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'laravel',
+			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+			'prefix'    => 'varaa_',
 		),
 
 		'pgsql' => array(
--- laravel/app/config/local/database.php
+++ laravel/app/config/local/database.php
@@ -23,12 +23,12 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'laravel',
+			'username'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+			'prefix'    => 'varaa_',
 		),
 
 		'pgsql' => array(
