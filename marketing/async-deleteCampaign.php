d0d66fb002ceac22bb9fb0416da895cd407fc811
Kris Jeni <jenistar90@gmail.com>
2014-08-10 21:17:51 +0700

    Ref #58

--- marketing/async-deleteCampaign.php
+++ marketing/async-deleteCampaign.php
@@ -1,19 +1,20 @@
 <?php
-	require_once("../DB_Connection.php");
-    require_once("common/functions.php");
+require_once("../DB_Connection.php");
+require_once("common/functions.php");
 
-    $result = "success";
-    $error = "";
-    $data = array();
-    
-    $campaignIds = mysql_escape_string($_POST['campaignIds']);
-    
-    
-    $sql = "delete from tbl_email_campaign where email_campaign in ($campaignIds)";
-    $db->query( $sql );
+$result = "success";
+$error = "";
+$data = array();
 
-    $data['result'] = $result;
-    $data['error'] = $error;
-    header('Content-Type: application/json');
-    echo json_encode($data);
+$campaignIds = mysql_escape_string($_POST['campaignIds']);
+
+$sql = "delete
+          from tbl_email_campaign
+         where email_campaign in ($campaignIds)";
+$db->query($sql);
+
+$data['result'] = $result;
+$data['error'] = $error;
+header('Content-Type: application/json');
+echo json_encode($data);
 ?>
