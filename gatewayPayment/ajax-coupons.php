704669a0977530bc6bbba019dfc31adeb8eef03c
Kris Jeni <jenistar90@gmail.com>
2014-08-12 17:09:40 +0700

    Ref #58 : Recurly

--- gatewayPayment/ajax-coupons.php
+++ gatewayPayment/ajax-coupons.php
@@ -1,68 +1,66 @@
 <?php
-  require_once('config.php');
-  require_once('lib/recurly.php'); 
-  Recurly_Client::$subdomain = RECURLY_SUBDOMAIN  ;
-  Recurly_Client::$apiKey = RECURLY_API_KEY;
-  Recurly_js::$privateKey = RECURLY_PRIVATE_KEY;  
-  $result = "success";
-  $error = "";
-  $data = array();
-  
-  $couponPost = $_POST["couponCode"];
-  $accountCode = $_POST["accountCode"];
-  $plancode = $_POST["plancode"]; 
-  
-  $coupons = Recurly_CouponList::get(); 
-  if($accountCode == ""){
-      $accountCoupon = "";
-  }else{ 
-     $account = Recurly_Account::get($accountCode); 
+require_once('config.php');
+require_once('lib/recurly.php');
+Recurly_Client::$subdomain = RECURLY_SUBDOMAIN  ;
+Recurly_Client::$apiKey = RECURLY_API_KEY;
+Recurly_js::$privateKey = RECURLY_PRIVATE_KEY;
+$result = "success";
+$error = "";
+$data = array();
+
+$couponPost = $_POST["couponCode"];
+$accountCode = $_POST["accountCode"];
+$plancode = $_POST["plancode"];
+
+$coupons = Recurly_CouponList::get();
+if ($accountCode == "") {
+    $accountCoupon = "";
+} else {
+    $account = Recurly_Account::get($accountCode);
     if ($account->redemption) {
-      $redemption = $account->redemption->get();
-      }
-      $redemption = $redemption->coupon._href ;
-      $arr = explode( ">", $redemption );  
-      $arr_code = explode( "/", $arr[0] ); 
-      $accountCoupon = $arr_code[count($arr_code)-1];
-      
-      $couponList = array();
-      foreach ($coupons as $coupon) { 
-           if ( !($coupon->state == "inactive" )){
-                  if( !($coupon->coupon_code == $accountCoupon)){
-                      if( $coupon->applies_to_all_plans=="true"){
-                           $couponList[] = $coupon->coupon_code;
-                       }else{
-                          for( $i=0;$i<count($coupon->plan_codes);$i++){
-                            if ( $coupon->plan_codes[$i]==$plancode) {
-                                   $couponList[] = $coupon->coupon_code;
-                              }
-                            }
-                         }  
-                 }                                
-           }
-      }
-      if( !in_array( $couponPost, $couponList ) ){
-           $result = "failed";
-      }
-      
-  }
-  if( $result != "failed" ){
-   try {
+        $redemption = $account->redemption->get();
+    }
+    $redemption = $redemption->coupon._href ;
+    $arr = explode(">", $redemption);
+    $arr_code = explode("/", $arr[0]);
+    $accountCoupon = $arr_code[count($arr_code)-1];
+
+    $couponList = array();
+    foreach ($coupons as $coupon) {
+        if (!($coupon->state == "inactive" )) {
+            if (!($coupon->coupon_code == $accountCoupon)) {
+                if ($coupon->applies_to_all_plans=="true") {
+                    $couponList[] = $coupon->coupon_code;
+                } else {
+                    for ($i = 0;$i < count($coupon->plan_codes); $i++) {
+                        if ($coupon->plan_codes[$i] == $plancode) {
+                            $couponList[] = $coupon->coupon_code;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (!in_array( $couponPost, $couponList)) {
+        $result = "failed";
+    }
+}
+if ($result != "failed") {
+    try {
         $coupon = Recurly_Coupon::get($couponPost);
-         if($coupon->discount_type == "percent"){
-           $coupontAmount = $coupon->discount_percent;
-           $couponType = "percent";
-         }else{
-           $coupontAmount = (array)$coupon->discount_in_cents->EUR->amount_in_cents ;
-           $couponType = CURRENCY_TYPE;  
-         }   
-     } catch (Recurly_NotFoundError $e) {
-          $result = "failed";
-     }
-     $data["couponType"] = $couponType;
-     $data["coupontAmount"] = $coupontAmount;
-  }
-  $data["result"] = $result ;
-  
-  echo json_encode($data);
+        if ($coupon->discount_type == "percent") {
+            $coupontAmount = $coupon->discount_percent;
+            $couponType = "percent";
+        } else {
+            $coupontAmount = (array)$coupon->discount_in_cents->EUR->amount_in_cents ;
+            $couponType = CURRENCY_TYPE;
+        }
+    } catch (Recurly_NotFoundError $e) {
+        $result = "failed";
+    }
+    $data["couponType"] = $couponType;
+    $data["coupontAmount"] = $coupontAmount;
+}
+$data["result"] = $result ;
+echo json_encode($data);
 ?>
--- gatewayPayment/hook.php
+++ gatewayPayment/hook.php
@@ -1,78 +1,71 @@
 <?php
-
-require_once('../marketing/common/DB_Connection.php');
-require_once('../marketing/common/functions.php');
+require_once("../DB_Connection.php");
 
 require_once('config.php');
-require_once('lib/recurly.php');         
+require_once('lib/recurly.php');
 Recurly_Client::$subdomain = RECURLY_SUBDOMAIN  ;
 Recurly_Client::$apiKey = RECURLY_API_KEY;
 Recurly_js::$privateKey = RECURLY_PRIVATE_KEY;
 
-    $post_xml = file_get_contents ("php://input"); 
-    $notification = new Recurly_PushNotification($post_xml);
-    //each webhook is defined by a type 
-    switch ($notification->type) {     
-      case "new_account_notification":
-      break;
-	  
-      case "new_subscription_notification":
-      break;
+$post_xml = file_get_contents ("php://input");
+$notification = new Recurly_PushNotification($post_xml);
+//each webhook is defined by a type
+switch ($notification->type) {
+    case "new_account_notification":
+        break;
+    case "new_subscription_notification":
+        break;
+    case "billing_info_updated_notification":
+        break;
+    case "successful_payment_notification":
+        $username = $notification->account->username;
+        $arr = explode(":", $username);
+        $ownerId = $arr[0];
+        $planCode = $arr[1];
+        $planGroupCode = substr($planCode, 0, 2);
+        $planPeriodType = substr($planCode, 2);
+         
+        $transactionId = $notification->transaction->id;
+        $amount = $notification->transaction->amount_in_cents / 100;
+        $accountCode = $notification->account->account_code;
+        $status = 'Y';
+        if ($planGroupCode == "sm") {
+            $expiredMonth = 12;
+        } elseif ($planPeriodType == "_month") {
+            $expiredMonth = 1;
+        } elseif ($planPeriodType == "_year") {
+            $expiredMonth = 12;
+        } else {
+            $expiredMonth = 0;
+        }
+        $expiredTime = $notification->subscription->current_period_ends_at;
 
-      case "billing_info_updated_notification":
-      break;
-
-      case "successful_payment_notification":
-      	$username = $notification->account->username;
-      	
-      	$arr = explode( ":", $username );
-      	$ownerId = $arr[0];
-      	$planCode = $arr[1];
-      	$planGroupCode = substr( $planCode, 0, 2 );
-      	$planPeriodType = substr( $planCode, 2 );
-      	
-      	$transactionId = $notification->transaction->id;
-      	$amount = $notification->transaction->amount_in_cents / 100;
-      	$accountCode = $notification->account->account_code;
-      	$status = 'Y';
-      	if( $planGroupCode == "sm" ){
-      		$expiredMonth = 12;
-      	}else if( $planPeriodType == "_month" ){
-      		$expiredMonth = 1;
-      	}else if( $planPeriodType == "_year" ){
-      		$expiredMonth = 12;
-      	}else{
-      		$expiredMonth = 0;
-      		
-      	}
-      	$expiredTime = $notification->subscription->current_period_ends_at;
-      	 
-      	$sql = "insert into tbl_payment_history( owner, plan_code, amount, transaction_id, account_code, status, created_time, updated_time)
-      			values( $ownerId, '$planCode', $amount, '$transactionId', '$accountCode', '$status', now(), now())";
-      	$db->queryInsert($sql);
-      	$sql = "select *
-      			  from tbl_owner_premium
-      			 where owner = '$ownerId'
-      			   and plan_group_code = '$planGroupCode'";
-      	$dataResult = $db->queryArray($sql);
-      	if( $planGroupCode == "mt" ){
-      		$credits = $amount * CREDITS_PRICE;      		
-      	}else{
-      		$credits = 0;
-      	}
-      	
-      	if( $dataResult == null ){
-	      	$sql = "insert into tbl_owner_premium( owner, plan_group_code, plan_code, account_code, credits, expired_time, updated_time)
-	      			values( $ownerId, '$planGroupCode', '$planCode', '$accountCode', $credits, date_add( now(), interval $expiredMonth month) , now())";
-	      	$db->queryInsert($sql);
-      	}else{
-	      	$sql = "update tbl_owner_premium
-	      			   set expired_time = date_add( expired_time, interval $expiredMonth month)
-	      				 , credits = credits + $credits
-	      			 where owner = '$ownerId'
-	      			   and plan_group_code = '$planGroupCode'";
-	      	$db->query($sql);
-      	}
-      break;
-    }
+        $sql = "insert into tbl_payment_history( owner, plan_code, amount, transaction_id, account_code, status, created_time, updated_time)
+                values ( $ownerId, '$planCode', $amount, '$transactionId', '$accountCode', '$status', now(), now())";
+        $db->queryInsert($sql);
+        $sql = "select *
+                  from tbl_owner_premium
+                 where owner = '$ownerId'
+                   and plan_group_code = '$planGroupCode'";
+        $dataResult = $db->queryArray($sql);
+        if ($planGroupCode == "mt") {
+            $credits = $amount * CREDITS_PRICE;
+        } else {
+            $credits = 0;
+        }
+         
+        if ($dataResult == null) {
+            $sql = "insert into tbl_owner_premium( owner, plan_group_code, plan_code, account_code, credits, expired_time, updated_time)
+                    values ( $ownerId, '$planGroupCode', '$planCode', '$accountCode', $credits, date_add( now(), interval $expiredMonth month) , now())";
+            $db->queryInsert($sql);
+        } else {
+            $sql = "update tbl_owner_premium
+                       set expired_time = date_add( expired_time, interval $expiredMonth month)
+                         , credits = credits + $credits
+                     where owner = '$ownerId'
+                       and plan_group_code = '$planGroupCode'";
+            $db->query($sql);
+        }
+        break;
+}
 ?>
