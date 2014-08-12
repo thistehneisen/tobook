f450a9bd2bea95efdddacc2a7c99bc8129515f1c
Quang Hung <hungnq1989@gmail.com>
2014-08-12 13:54:26 +0300

    #53 cannot select overlapp freetime, lunch, green box is selectable slot

--- appointment/library/app/web/js/pjAppScheduler.js
+++ appointment/library/app/web/js/pjAppScheduler.js
@@ -141,7 +141,7 @@
 					var objP = objK;
 					for( var p = 0; p < nextCnt; p ++ ){
 						if( objP.hasClass("asSlotUnavailable") ){
-							objK.addClass("asSlotUnselectable");
+							//objK.addClass("asSlotUnselectable");
 							continue;
 						}
 						objP = objP.next();
@@ -511,9 +511,9 @@
 				// modified by jeni : 2014-07-17
 				var objCurrent1 = pjQ.$(this);
 				var objCurrent2 = pjQ.$(this);
-                if(objCurrent2.hasClass('asSlotUnselectable')){
-                    return;
-                }
+                // if(objCurrent2.hasClass('asSlotUnselectable')){
+                //     return;
+                // }
 				var arr = pjQ.$("div.asServiceTime").eq(0).text();
 				arr = arr.split(" ");
 				var minute = arr[0];
