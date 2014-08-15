0222ada935bd5f9979f1a4fe99ee09c0fc580ad9
An Cao <kcj.pop.com@gmail.com>
2014-08-15 12:06:51 +0300

    Add Mandrill config.

--- laravel/app/config/mail.php
+++ laravel/app/config/mail.php
@@ -15,7 +15,7 @@ return array(
 	|
 	*/
 
-	'driver' => 'smtp',
+	'driver' => 'mandrill',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.mandrillapp.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'no-reply@klikkaaja.com', 'name' => 'Klikkaaja'),
 
 	/*
 	|--------------------------------------------------------------------------
--- laravel/app/config/services.php
+++ laravel/app/config/services.php
@@ -20,7 +20,7 @@ return array(
 	),
 
 	'mandrill' => array(
-		'secret' => '',
+		'secret' => 'CTzo04IR5HYpn9LjCGV-7A',
 	),
 
 	'stripe' => array(
