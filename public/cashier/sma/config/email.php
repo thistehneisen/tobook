4066feea592cd7a8af0501d06c99f66597df3ca6
kcjpop <kcj.pop.com@gmail.com>
2014-08-16 14:28:55 +0300

    Use L4 email settings in CS.

--- public/cashier/sma/config/email.php
+++ public/cashier/sma/config/email.php
@@ -1,9 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-require_once realpath(BASEPATH.'../../../includes/configsettings.php');
+require_once realpath(BASEPATH.'../../../Bridge.php');
+$varaaEmail = Bridge::emailConfig();
 
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = SMTP_HOST;
-$config['smtp_user'] = SMTP_USERNAME;
-$config['smtp_pass'] = SMTP_PASSWORD;
-$config['smtp_port'] = SMTP_PORT;
+$config['smtp_host'] = $varaaEmail['host'];
+$config['smtp_port'] = $varaaEmail['port'];
+$config['smtp_user'] = $varaaEmail['username'];
+$config['smtp_pass'] = $varaaEmail['password'];
 $config['smtp_timeout'] = 5;
