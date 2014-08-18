d6ccd576c7467393d5125cd65ca0dcb4191d7206
Tung Nguyen <sontung1907@gmail.com>
2014-08-18 19:39:33 +0300

    #55 : Stuck but just commit.

--- app/tests/acceptance/AppointmentSchedulerCest.php
+++ app/tests/acceptance/AppointmentSchedulerCest.php
@@ -12,6 +12,7 @@ class AppointmentSchedulerCest {
         ]);
         $I->seeInCurrentUrl('dashboard');
         $I->amOnPage('/services/appointment-scheduler');
+        //$I->amOnPage('http://varaal4.dev/appointment/index.php?username=capis&owner_id=1');
     }
 
     public function _after() {
@@ -23,7 +24,31 @@ class AppointmentSchedulerCest {
     }
 
     public function testTabs(AcceptanceTester $I) {
-        //$I->amOnPage('/appointment/index.php?username=capis&owner_id=1');
-        $I->seeLink('Kalenteri');
+        //$I->amOnPage("/appointment/index.php?controller=pjAdminServices&action=pjActionIndex&as_pf=");
+        //$I->click('Lisää kategoria');
+        //$I->see('Kalenteri');
+        //$I->switchToIFrame("varaa-iframe");
+    }
+
+    public function TestRedirect(AcceptanceTester $I) {
+        /*$I->amOnPage('/appointment/index.php?controller=pjAdmin&action=pjActionIndex&as_pf=');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
+        $I->amOnPage('/appointment/index.php?controller=pjAdminBookings&action=pjActionIndex&as_pf=');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
+        $I->amOnPage('/appointment/index.php?controller=pjAdminServices&action=pjActionIndex&as_pf=');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
+        $I->amOnPage('/appointment/index.php?controller=pjAdminEmployees&action=pjActionIndex&as_pf=');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
+        $I->amOnPage('/appointment/index.php?controller=pjAdminOptions&action=pjActionIndex&tab=1&as_pf=');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
+        $I->amOnPage('/appointment/index.php?controller=pjAdminReports&action=pjActionEmployees&as_pf=');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
+        $I->amOnPage('http://varaal4.dev/appointment/index.php?controller=pjAdminOptions&action=pjActionInstall&as_pf=');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');*/
+        //$I->amOnPage('/appointment/index.php?controller=pjAdminOptions&action=pjActionPreview&as_pf=&owner_id=1');
+        $I->amOnPage('/appointment/library/index.php?controller=pjAdmin&action=pjActionIndex&as_pf=&owner_id=1');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
+        $I->amOnPage('/appointment/library/index.php?controller=pjAdminOptions&action=pjActionPreview&as_pf=&owner_id=1');
+        $I->see('Ajanvarauksen tarjoaa varaa.com');
     }
 }
