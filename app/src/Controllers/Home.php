5b00ebaae97160affc4bf5a8a40507a20b0c4389
Hieu Nguyen <hieuh25@gmail.com>
2014-08-19 15:20:10 +0300

    Fix home + htaccess

--- app/src/Controllers/Home.php
+++ app/src/Controllers/Home.php
@@ -7,10 +7,6 @@ class Home extends Base
 {
     public function index()
     {
-        if (Confide::user()) {
-            return View::make('home.list');
-        }
-
         return View::make('home.index');
     }
 
