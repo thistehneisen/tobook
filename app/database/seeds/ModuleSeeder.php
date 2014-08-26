24c51acf7e6e3e0cd1bff83d559e49e668df961e
kcjpop <kcj.pop.com@gmail.com>
2014-08-26 20:07:20 +0300

    Activate all services for all users in 100 years.

---
+++ app/database/seeds/ModuleSeeder.php
@@ -0,0 +1,47 @@
+<?php
+use Carbon\Carbon;
+use App\Core\Models\Module;
+use App\Core\Models\User;
+
+class ModuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $data = [
+            ['name' => 'appointment', 'uri' => '/services/appointment-scheduler'],
+            ['name' => 'cashier', 'uri' => '/services/cashier'],
+            ['name' => 'restaurant', 'uri' => '/services/restaurant-booking'],
+            ['name' => 'timeslot', 'uri' => '/services/timeslot'],
+            ['name' => 'loyalty', 'uri' => '/services/loyalty-program'],
+        ];
+
+        $modules = [];
+        foreach ($data as $item) {
+            $module = new Module($item);
+            $module->save();
+
+            $modules[] = $module->id;
+        }
+
+        // All users
+        $users = User::all();
+        $start = Carbon::now();
+        $end   = Carbon::now()->addYears(100);
+
+        foreach ($users as $user) {
+            echo "Now processing {$user->email}...\n";
+            $user->modules()->attach($modules, [
+                'start'     => $start,
+                'end'       => $end,
+                'is_active' => true
+            ]);
+        }
+    }
+
+}
