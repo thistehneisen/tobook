be29de48582bfb934cb53fbf25eda3d8edbcb06a
kcjpop <kcj.pop.com@gmail.com>
2014-08-28 02:06:42 +0300

    Add acceptance test for AS Services.

---
+++ app/tests/Elements/Appointment.php
@@ -0,0 +1,10 @@
+<?php namespace Test\Elements;
+
+class Appointment
+{
+    // Services
+    public static $serviceUrl = '/appointment-scheduler/services';
+    public static $serviceCategoryUrl = '/appointment-scheduler/services/categories';
+    public static $serviceResourceUrl = '/appointment-scheduler/services/resources';
+    public static $serviceExtraServiceUrl = '/appointment-scheduler/services/extras';
+}
