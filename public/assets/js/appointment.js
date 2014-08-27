33047f8197ddaa778f71021150f9c3bf0a98341f
Quang Hung <hungnq1989@gmail.com>
2014-08-27 21:36:09 +0300

    Add linted JS appointment.js

--- public/assets/js/appointment.js
+++ public/assets/js/appointment.js
@@ -1,49 +1,53 @@
-$(document).ready(function () {
-    var spinner = $( ".spinner" ).spinner();
-    $('.toggle-check-all-boxes').click(function(e){
-        var checkboxClass = ($(this).data('checkbox-class')) || 'checkbox';
-        $('.' + checkboxClass).prop('checked', this.checked);
+(function ($) {
+  $(function () {
+    'use strict';
+    // Add spinner to form
+    $(".spinner").spinner();
+    $('.toggle-check-all-boxes').click(function () {
+      var checkboxClass = ($(this).data('checkbox-class')) || 'checkbox';
+      $('.' + checkboxClass).prop('checked', this.checked);
     });
 
-    $('#form-bulk').on('submit', function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        alertify.confirm($this.data('confirm'), function (e) {
-            if (e) {
-                // user clicked "ok"
-                $.ajax({
-                    type: 'POST',
-                    url: $this.attr('action'),
-                    data: $this.serialize(),
-                    dataType: 'json'
-                }).done(function(data) {
-                    alertify.alert('OK');
-                    if($('#mass-action').val()=='destroy'){
-                        $("#form-bulk [type=checkbox]:checked").each(function(){
-                            $('#row-'+$(this).val()).remove();
-                        });
-                    }
-                }).fail(function() {
-                    alertify.alert('Something went wrong');
-                });
+    $('#form-bulk').on('submit', function (e) {
+      e.preventDefault();
+      var $this = $(this);
+      alertify.confirm($this.data('confirm'), function (e) {
+        if (e) {
+          // user clicked "ok"
+          $.ajax({
+            type: 'POST',
+            url: $this.attr('action'),
+            data: $this.serialize(),
+            dataType: 'json'
+          }).done(function () {
+            alertify.alert('OK');
+            if ($('#mass-action').val() === 'destroy') {
+              $("#form-bulk [type=checkbox]:checked").each(function () {
+                $('#row-' + $(this).val()).remove();
+              });
             }
-        });
+          }).fail(function() {
+            alertify.alert('Something went wrong');
+          });
+        }
+      });
     });
 
     // Allow to click on TR to select checkbox
-    $('table.table-crud tr').on('click', function(event) {
-        var target = $(event.target);
-        if(target.is('td')){//fix bug cannot click to the actuall checkbox
-            var $this = $(this),
-                checkbox = $this.find('td:first input:checkbox'),
-                checked = checkbox.prop('checked');
+    $('table.table-crud tr').on('click', function (event) {
+      var target = $(event.target);
+      if (target.is('td')) {//fix bug cannot click to the actual checkbox
+        var $this = $(this),
+          checkbox = $this.find('td:first input:checkbox'),
+          checked = checkbox.prop('checked');
 
-            checkbox.prop('checked', !checked);
-        }
+        checkbox.prop('checked', !checked);
+      }
     });
 
     // Date picker
     $('.date-picker').datepicker({
-        format: 'yyyy-mm-dd'
+      format: 'yyyy-mm-dd'
     });
- });
+  });
+}(jQuery));
