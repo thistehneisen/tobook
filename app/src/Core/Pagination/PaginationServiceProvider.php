6d39406183d6806521023a378b4f0ede9a77ad68
kcjpop <kcj.pop.com@gmail.com>
2014-09-07 12:15:59 +0300

    Append current query params in i18n pagination URLs

---
+++ app/src/Core/Pagination/PaginationServiceProvider.php
@@ -0,0 +1,22 @@
+<?php namespace App\Core\Pagination;
+
+class PaginationServiceProvider extends \Illuminate\Pagination\PaginationServiceProvider
+{
+    /**
+     * @{@inheritdoc}
+     */
+    public function register()
+    {
+        $this->app->bindShared('paginator', function($app)
+        {
+            $paginator = new Factory($app['request'], $app['view'], $app['translator']);
+            $paginator->setViewName($app['config']['view.pagination']);
+
+            // Support i18n URLs
+            $paginator->setBaseUrl($app->getLocale().'/'.$app['request']->path());
+            $app->refresh('request', $paginator, 'setRequest');
+
+            return $paginator;
+        });
+    }
+}
