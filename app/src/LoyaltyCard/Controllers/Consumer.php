ba0c1f81275bebe92e87d418c576813e6d536a10
Tung Nguyen <sontung1907@gmail.com>
2014-09-07 04:13:14 +0300

    #87: Fix get consumers

--- app/src/LoyaltyCard/Controllers/Consumer.php
+++ app/src/LoyaltyCard/Controllers/Consumer.php
@@ -18,6 +18,8 @@ class Consumer extends Base
     private function viewIndex($isApp = false, $search = null) {
         if ($search != null) {
             $consumers = Model::join('consumers', 'lc_consumers.consumer_id', '=', 'consumers.id')
+                            ->join('consumer_user', 'lc_consumers.consumer_id', '=', 'consumer_user.consumer_id')
+                            ->where('consumer_user.user_id', Confide::user()->id)
                             ->where('consumers.first_name', 'like', '%' . $search . '%')
                             ->orWhere('consumers.last_name', 'like', '%' . $search . '%')
                             ->orWhere('consumers.email', 'like', '%' . $search . '%')
@@ -26,6 +28,8 @@ class Consumer extends Base
                             ->paginate(10);
         } else {
             $consumers = Model::join('consumers', 'lc_consumers.consumer_id', '=', 'consumers.id')
+                            ->join('consumer_user', 'lc_consumers.consumer_id', '=', 'consumer_user.consumer_id')
+                            ->where('consumer_user.user_id', Confide::user()->id)
                             ->select('lc_consumers.id', 'consumers.first_name', 'consumers.last_name', 'consumers.email', 'consumers.phone', 'lc_consumers.updated_at')
                             ->paginate(10);
         }
