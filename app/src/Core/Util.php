af12b04e1a62eb67bee8ea54fb6e6784e1baf0a4
Hieu Nguyen <hieuh25@gmail.com>
2014-09-09 16:39:09 +0300

    Add util method pp() to replace var_dump() and print_r()

--- app/src/Core/Util.php
+++ app/src/Core/Util.php
@@ -51,4 +51,19 @@ class Util
         }
         return $dayOfWeek;
     }
+
+    /**
+     * pp()
+     * Helper function to extend the standard debug function print_r()
+     * Note: if you want to debug many variables at the same time, wrap them in an array: printr(array($var1, $var2, $var3));
+     *
+     * @param mixed $var to be debugged
+     * @param bool $hidden is false by default, if $hidden is true then the output is hidden
+     * @return null
+     **/
+    public static function pp($var, $hidden = false) {
+        $display = '';
+        if ($hidden) $display = 'style="display:none;"';
+        echo "<pre {$display}>".var_export($var, true)."</pre>";
+    }
 }
