40abd14ae58f97fc241e4934cc401911b6c360c6
kcjpop <kcj.pop.com@gmail.com>
2014-09-14 01:40:03 +0300

    Fix missing euro sign.

--- app/views/modules/as/bookings/stat/monthly.blade.php
+++ app/views/modules/as/bookings/stat/monthly.blade.php
@@ -29,9 +29,9 @@
                     <p>{{ trans('as.bookings.stat.revenue') }} <span class="pull-right">&euro;{{ $report['revenue'] }}
                         @if (isset($report['gap']['revenue']))
                             @if ($report['gap']['revenue'] > 0)
-                                <small class="text-success"><i class="fa fa-arrow-up"></i> <strong>{{ $report['gap']['revenue'] }}</strong></small>
+                                <small class="text-success"><i class="fa fa-arrow-up"></i> <strong>&euro;{{ $report['gap']['revenue'] }}</strong></small>
                             @elseif (($report['gap']['revenue'] < 0))
-                                <small class="text-danger"><i class="fa fa-arrow-down"></i> <strong>{{ $report['gap']['revenue'] }}</strong></small>
+                                <small class="text-danger"><i class="fa fa-arrow-down"></i> <strong>&euro;{{ $report['gap']['revenue'] }}</strong></small>
                             @endif
                         @endif
                     </span></p>
