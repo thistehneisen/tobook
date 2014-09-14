11b94ee9f57d907e45b97c4643718ab077ae65e3
kcjpop <kcj.pop.com@gmail.com>
2014-09-14 15:30:22 +0300

    Init tab structure of FD home.

--- app/src/FlashDeal/Controllers/Index.php
+++ app/src/FlashDeal/Controllers/Index.php
@@ -1,13 +1,50 @@
 <?php namespace App\FlashDeal\Controllers;
 
+use App;
 use App\Core\Controllers\Base;
 
 class Index extends Base
 {
     protected $viewPath = 'modules.fd.index';
 
-    public function index()
+    public function index($tab = null)
     {
-        return $this->render('index');
+        $tab = $tab ?: 'sold-flash-deals';
+        $method = camel_case($tab);
+        if (!method_exists($this, $method)) {
+            App::abort(404);
+        }
+
+        $content = $this->$method();
+        return $this->render('index', [
+            'tab'     => $tab,
+            'content' => $content
+        ]);
     }
+
+    public function soldFlashDeals()
+    {
+        return 'hehe';
+    }
+
+    public function soldCoupons()
+    {
+        return 'hehe';
+    }
+
+    public function activeFlashDeals()
+    {
+        return 'hehe';
+    }
+
+    public function activeCoupons()
+    {
+        return 'hehe';
+    }
+
+    public function expired()
+    {
+        return 'hehe';
+    }
+
 }
--- app/views/modules/fd/index/tabs.blade.php
+++ app/views/modules/fd/index/tabs.blade.php
@@ -1,9 +1,9 @@
 <!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
-  <li class="active"><a href="#sold-flash-deals" role="tab" data-toggle="tab">Sold flash deals</a></li>
-  <li><a href="#sold-coupons" role="tab" data-toggle="tab">Sold coupons</a></li>
-  <li><a href="#active-flash-deals" role="tab" data-toggle="tab">Active flash deals</a></li>
-  <li><a href="#active-coupons" role="tab" data-toggle="tab">Active coupons</a></li>
-  <li><a href="#expired" role="tab" data-toggle="tab">Expired coupons/flash deals</a></li>
+    <li class="{{ $tab === 'sold-flash-deals' ? 'active' : '' }}"><a href="{{ route('fd.index', ['tab' => 'sold-flash-deals']) }}" role="tab">Sold flash deals</a></li>
+    <li class="{{ $tab === 'sold-coupons' ? 'active' : '' }}"><a href="{{ route('fd.index', ['tab' => 'sold-coupons']) }}" role="tab">Sold coupons</a></li>
+    <li class="{{ $tab === 'active-flash-deals' ? 'active' : '' }}"><a href="{{ route('fd.index', ['tab' => 'active-flash-deals']) }}" role="tab">Active flash deals</a></li>
+    <li class="{{ $tab === 'active-coupons' ? 'active' : '' }}"><a href="{{ route('fd.index', ['tab' => 'active-coupons']) }}" role="tab">Active coupons</a></li>
+    <li class="{{ $tab === 'expired' ? 'active' : '' }}"><a href="{{ route('fd.index', ['tab' => 'expired']) }}" role="tab">Expired coupons/flash deals</a></li>
 </ul>
 <br>
