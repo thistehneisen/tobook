805cf43faa3527ad33dc11454b105c03ea053cab
Quang Hung <hungnq1989@gmail.com>
2014-09-16 02:33:48 +0300

    Fix display wrong class for custom time in FE

--- app/src/Appointment/Models/Slot/Frontend.php
+++ app/src/Appointment/Models/Slot/Frontend.php
@@ -41,9 +41,11 @@ class Frontend implements Strategy
         foreach ($this->customTimeCache as $empCustomTime) {
             $startAt =  Carbon::createFromFormat('H:i:s', $empCustomTime->customTime->start_at, Config::get('app.timezone'));
             $endAt   =  Carbon::createFromFormat('H:i:s', $empCustomTime->customTime->end_at, Config::get('app.timezone'));
-            if (($rowTime >= $startAt && $rowTime <= $endAt) || (bool)$empCustomTime->customTime->is_day_off) {
-                $class = 'custom_time';
+            if ($rowTime >= $startAt && $rowTime <= $endAt && !$empCustomTime->customTime->is_day_off) {
+                $class = 'active';
                 $this->customTimeSlot[$selectedDate->toDateString()][(int) $hour][(int) $minute] = $empCustomTime;
+            } else {
+                $class = 'custom inactive';
             }
         }
 
