aa6a143a0e4cce4090d26a7c14bb18fe1f003353
Quang Hung <hungnq1989@gmail.com>
2014-09-17 01:23:18 +0300

    Migration for change columns which holds minutes from tinyint to int(10)

---
+++ app/database/migrations/as/2014_09_17_011420_change_tiny_int_to_integer_for_some_columns.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTinyIntToIntegerForSomeColumns extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        //Bookings
+        DB::statement("ALTER TABLE `varaa_as_bookings` CHANGE `total` `total` int(10) unsigned NULL default 0");
+
+        //Services
+        DB::statement("ALTER TABLE `varaa_as_services` CHANGE `length` `length` int(10) unsigned NULL default 0");
+        DB::statement("ALTER TABLE `varaa_as_services` CHANGE `before` `before` int(10) unsigned NULL default 0");
+        DB::statement("ALTER TABLE `varaa_as_services` CHANGE `during` `during` int(10) unsigned NULL default 0");
+        DB::statement("ALTER TABLE `varaa_as_services` CHANGE `after` `after` int(10) unsigned NULL default 0");
+
+        //Service times
+        DB::statement("ALTER TABLE `varaa_as_service_times` CHANGE `length` `length` int(10) unsigned NULL default 0");
+        DB::statement("ALTER TABLE `varaa_as_service_times` CHANGE `before` `before` int(10) unsigned NULL default 0");
+        DB::statement("ALTER TABLE `varaa_as_service_times` CHANGE `during` `during` int(10) unsigned NULL default 0");
+        DB::statement("ALTER TABLE `varaa_as_service_times` CHANGE `after` `after` int(10) unsigned NULL default 0");
+
+        //Resources
+        DB::statement("ALTER TABLE `varaa_as_resources` CHANGE `quantity` `quantity` int(10) unsigned NULL default 0");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
