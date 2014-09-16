a4aafb4bfaed33370c1a6db4a98a8620cbd55fdf
Tung Nguyen <sontung1907@gmail.com>
2014-09-17 00:59:22 +0300

    #106: Add/Update consumer

--- app/src/Consumers/Controllers/Consumer.php
+++ app/src/Consumers/Controllers/Consumer.php
@@ -1,5 +1,6 @@
 <?php namespace App\Consumers\Controllers;
 
+use Input;
 use App\Core\Controllers\Base;
 use App\Appointment\Traits\Crud;
 
@@ -8,4 +9,14 @@ class Consumer extends Base
     use Crud;
     protected $modelClass = 'App\Consumers\Models\Consumer';
     protected $langPrefix = 'co';
+
+    protected function upsertHandler($item)
+    {
+        $item->saveOrFail();
+        $item->fill(Input::all());
+        $item->users()->detach($this->user->id);
+        $item->users()->attach($this->user, ['is_visible' => true]);
+
+        return $item;
+    }
 }
