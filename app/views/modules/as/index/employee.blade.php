dec719a8de8fd71cc511b38fc617cc83219b2e78
Quang Hung <hungnq1989@gmail.com>
2014-09-17 23:17:16 +0300

    Use getIsActive() instead of cast is_active to int

--- app/views/modules/as/index/employee.blade.php
+++ app/views/modules/as/index/employee.blade.php
@@ -31,7 +31,7 @@
 <div class="container-fluid row-no-padding">
     <ul class="nav nav-tabs" role="tablist">
         @foreach ($employees as $employee)
-            @if (intval($employee->is_active) === 1)
+            @if ($employee->getIsActive())
             <li class="@if(intval($employee->id) === intval($employeeId)) active @endif">
                 <a href="{{ route('as.employee', ['id'=> $employee->id ]) }}">{{ $employee->name }}</a>
             </li>
