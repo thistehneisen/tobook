d9fc525d2c49b0bc9a51ff84dc4879f743817dcf
An Cao <kcj.pop.com@gmail.com>
2014-09-21 02:27:50 +0300

    Hide business tabs in user profile.

--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -32,8 +32,10 @@ $(function () {
         <!-- Nav tabs -->
         <ul class="nav nav-tabs" role="tablist">
             <li {{ $activeTab === 'general' ? 'class="active"' : '' }}><a href="#tab-general" role="tab" data-toggle="tab">{{ trans('user.profile.general') }}</a></li>
+        @if (Confide::user()->is_consumer === false)
             <li {{ $activeTab === 'business' ? 'class="active"' : '' }}><a href="#tab-business" role="tab" data-toggle="tab">{{ trans('user.profile.business') }}</a></li>
             <li {{ $activeTab === 'images' ? 'class="active"' : '' }}><a href="#tab-images" role="tab" data-toggle="tab">{{ trans('user.profile.images') }}</a></li>
+        @endif
             <li {{ $activeTab === 'password' ? 'class="active"' : '' }}><a href="#tab-password" role="tab" data-toggle="tab">{{ trans('user.change_password') }}</a></li>
         </ul>
         <br>
@@ -45,6 +47,7 @@ $(function () {
                 @include ('user.el.general')
             </div> <!-- General information -->
 
+        @if (Confide::user()->is_consumer === false)
             <div class="tab-pane {{ $activeTab === 'business' ? 'active' : '' }}" id="tab-business">
                 @include ('user.el.business')
             </div> <!-- Images -->
@@ -52,6 +55,7 @@ $(function () {
             <div class="tab-pane {{ $activeTab === 'images' ? 'active' : '' }}" id="tab-images">
                 @include ('user.el.images')
             </div> <!-- Images -->
+        @endif
 
             <div class="tab-pane {{ $activeTab === 'password' ? 'active' : '' }}" id="tab-password">
                 @include ('user.el.password')
