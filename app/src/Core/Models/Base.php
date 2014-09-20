1007454b7682613d5d42ecba5c08836f8d1946a6
Tung Nguyen <sontung1907@gmail.com>
2014-09-20 22:26:31 +0300

    Fix Base Model

--- app/src/Core/Models/Base.php
+++ app/src/Core/Models/Base.php
@@ -20,14 +20,18 @@ class Base extends \Eloquent
     public function scopeOfUser($query, $userId)
     {
         $table = $this->getTable();
+
         if (method_exists($this, 'user')) {
-            return $query->where($table . '.' . 'user_id', $userId);
+            return $query->where($table.'.user_id', $userId);
         } elseif (method_exists($this, 'users')) {
-            return $query->whereHas('users', function($q) use ($userId) {
-                return $q->where($table . '.' . 'user_id', $userId);
+            if ($this->users() instanceof \Illuminate\Database\Eloquent\Relations\BelongsToMany) {
+                $table = $this->users()->getTable();
+            }
+
+            return $query->whereHas('users', function($query) use ($userId, $table) {
+                return $query->where($table.'.user_id', $userId);
             });
         }
-
 
         return $query;
     }
