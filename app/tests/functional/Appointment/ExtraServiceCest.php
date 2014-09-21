5a1bfc53c3af03f4e40ca6c02ca1f61b9d015a05
An Cao <kcj.pop.com@gmail.com>
2014-09-22 00:57:22 +0300

    Refactor and test upser of AS extra service.

--- app/tests/functional/Appointment/ExtraServiceCest.php
+++ app/tests/functional/Appointment/ExtraServiceCest.php
@@ -4,12 +4,31 @@ use Test\Functional\Base;
 
 class ExtraServiceCest extends Base
 {
+    const INDEX  = '/appointment-scheduler/services/extras';
+    const UPSERT = '/appointment-scheduler/services/extras/upsert';
     /**
      * @before login
      */
-    public function seeIndexPage(\FunctionalTester $i)
+    public function seePages($i)
     {
-        $i->amOnPage('/appointment-scheduler/services/extras');
+        $i->amOnPage(self::INDEX);
         $i->seeResponseCodeIs(200);
+        $i->amOnPage(self::UPSERT);
+        $i->seeResponseCodeIs(200);
+    }
+
+    /**
+     * @before login
+     */
+    public function addNewExtraServices($i)
+    {
+        $i->amOnPage(self::UPSERT);
+        $i->fillField (['name' => 'name'], 'Foo');
+        $i->fillField (['name' => 'price'], 199);
+        $i->fillField (['name' => 'length'], 15);
+        $i->fillField (['name' => '_token'], csrf_token());
+        $i->click('#form-olut-upsert button[type=submit]');
+
+        $i->haveRecord('as_extra_services', ['name' => 'Foo']);
     }
 }
