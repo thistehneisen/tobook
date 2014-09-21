9bbe49d97c193e9eb7060cc6b6f1ffb474d0c220
An Cao <kcj.pop.com@gmail.com>
2014-09-21 23:03:45 +0300

    Add new CRUD to Extra services.

--- app/src/Appointment/Controllers/ExtraServices.php
+++ app/src/Appointment/Controllers/ExtraServices.php
@@ -1,10 +1,33 @@
 <?php namespace App\Appointment\Controllers;
 
-use App, View, Confide, Redirect, Input, Config, Response;
+use Input, Confide;
 
 class ExtraServices extends AsBase
 {
-    use App\Appointment\Traits\Crud;
+    use \CRUD;
 
-    protected $langPrefix = 'as.services.extras';
+    protected $crudOptions = [
+        'langPrefix' => 'as.services.extras',
+        'modelClass' => 'App\Appointment\Models\ExtraService',
+        'layout'     => 'modules.as.layout',
+        'lomake' => [
+            'length' => [
+                'type' => 'Spinner',
+                'values' => 10,
+                'options' => ['class' => 'form-control input-sm spinner', 'data-positive' => 'true', 'data-inc' => 5]
+            ],
+        ]
+    ];
+
+    /**
+     * @{@inheritdoc}
+     */
+    public function upsertHandler($item)
+    {
+        $item->fill(Input::all());
+        $item->user()->associate(Confide::user());
+        $item->saveOrFail();
+
+        return $item;
+    }
 }
