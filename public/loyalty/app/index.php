a5e0b974541ed85ef7e7e0a8ee4cfccbbe555d74
Hieu Nguyen <hieuh25@gmail.com>
2014-10-04 03:29:52 +0300

    Remove old LC

--- public/loyalty/app/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	if (isset($_COOKIE['CUSTOMER_TOKEN'])) {
-		header("location: main.php");
-	} else {
-		header("location: login.php");
-	}
-?>
--- public/loyalty/common/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function logToFile($filename, $msg)
-	{
-		$fd = fopen($filename, "a");
-		$str = "[" . date("Y/m/d h:i:s", time()) . "] " . $msg;
-		fwrite($fd, $str . "\n");
-		fclose($fd);
-	}	
-?>
