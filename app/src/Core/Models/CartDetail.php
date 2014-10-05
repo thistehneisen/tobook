41d9a07b24ece315a6a66ec6e9d5467a4223fe68
Quang Hung <hungnq1989@gmail.com>
2014-10-05 19:29:01 +0300

    Modify Cart and CartDetail model

--- app/src/Core/Models/CartDetail.php
+++ app/src/Core/Models/CartDetail.php
@@ -1,8 +1,47 @@
 <?php namespace App\Core\Models;
+use App\Appointment\Models\BookingService;
 
 class CartDetail extends Base
 {
      public $fillable = ['item', 'variant', 'quantity', 'price'];
+
+
+    public static function make($data, $cart)
+    {
+        try {
+            $detail = new self();
+            $detail->fill($data);
+            $detail->cart()->associate($cart);
+            $detail->save();
+        } catch (\Illuminate\Database\QueryException $ex) {
+            throw $ex;
+        }
+
+        return $detail;
+    }
+
+    public function getASBookingInfo()
+    {
+        $bookingService = BookingService::find($this->item);
+        $bookingService->calculateExtraServices();
+
+        $price = (!empty($bookingService->serviceTime))
+            ? $bookingService->serviceTime->price
+            : $bookingService->service->price;
+
+        $price += $bookingService->getExtraServicePrice();
+
+        $data = [
+            'datetime'      => $bookingService->date,
+            'price'         => $price,
+            'service_name'  => $bookingService->service->name,
+            'employee_name' => $bookingService->employee->name,
+            'start_at'      => $bookingService->getCartStartAt()->format('H:i'),
+            'end_at'        => $bookingService->getCartEndAt()->format('H:i'),
+            'uuid'          => $bookingService->tmp_uuid
+        ];
+        return $data;
+    }
 
     //--------------------------------------------------------------------------
     // RELATIONSHIPS
@@ -10,10 +49,5 @@ class CartDetail extends Base
     public function cart()
     {
         return $this->belongsTo('App\Core\Models\Cart');
-    }
-
-    public function consumer()
-    {
-       return $this->belongsTo('App\Consumers\Models\Consumer');
     }
 }
