d941f39e23178c764993ba8666f24832122d432e
An Cao <kcj.pop.com@gmail.com>
2014-10-10 15:48:17 +0300

    Init method to process Skrill notify request.

--- app/src/Payment/Gateways/Skrill.php
+++ app/src/Payment/Gateways/Skrill.php
@@ -1,7 +1,7 @@
 <?php namespace App\Payment\Gateways;
 
 use Omnipay\Omnipay;
-use Config, App;
+use Config, App, Validator;
 
 class Skrill extends Base
 {
@@ -28,7 +28,9 @@ class Skrill extends Base
             'amount'        => $transaction->amount,
             'currency'      => $transaction->currency,
             'transactionId' => $transaction->id,
-            'details'       => [],
+            'notifyUrl'     => route('payment.notify', ['gateway' => 'skrill']),
+            // TODO: Populate with cart details
+            'details'       => ['foo' => 'bar'],
         ];
 
         return $this->gateway->purchase($options)->send();
@@ -40,5 +42,74 @@ class Skrill extends Base
     public function success($response)
     {
         dd($response);
+    }
+
+    /**
+     * Handle notify request from Skrill server
+     *
+     * @see Skrill Payment Gateway Integration Guide (v6.8)
+     * @return string
+     */
+    public function notify()
+    {
+        // Validate data first
+        $v = Validator::make(Input::all(), [
+            'pay_to_email'   => 'required',
+            'merchant_id'    => 'required',
+            'transaction_id' => 'required',
+            'mb_amount'      => 'required',
+            'mb_currency'    => 'required',
+            'status'         => 'required',
+            'md5sig'         => 'required',
+        ]);
+
+        if ($v->fails()) {
+            return App::abort(400);
+        }
+
+        // Validate logic
+        if ($this->isValidRequest() === false
+            || $this->isValidEmail() === false
+            || $this->isValidAmount === false) {
+            return App::abort(400);
+        }
+
+        // Log this request for later investigation
+        // Get transaction
+        // Update relevant data
+
+        // Complete, exit to prevent any additional output
+        exit;
+    }
+
+    /**
+     * Validate this request really comes from Skrill server
+     *
+     * @return boolean
+     */
+    protected function isValidRequest()
+    {
+        // @todo
+        return false;
+    }
+
+    /**
+     * Validate payment was sent to correct email
+     *
+     * @return boolean
+     */
+    protected function isValidEmail()
+    {
+        return Input::get('pay_to_email') !== Config::get('services.skrill.email');
+    }
+
+    /**
+     * Validate payment is a correct amount
+     *
+     * @return boolean
+     */
+    protected function isValidAmount()
+    {
+        return false;
     }
 }
--- app/src/Payment/routes.php
+++ app/src/Payment/routes.php
@@ -6,7 +6,7 @@
 */
 Route::group([
     'prefix' => 'payment'
-], function() {
+], function () {
 
     Route::get('/', [
         'as' => 'payment.index',
@@ -15,12 +15,18 @@ Route::group([
 
     Route::get('test', function () {
         $cart = Cart::make([], 63);
+
         return Payment::redirect($cart, 1);
     });
 
     Route::post('purchase', [
         'as' => 'payment.purchase',
         'uses' => 'App\Payment\Controllers\Index@purchase'
+    ]);
+
+    Route::post('notify/{gateway}', [
+        'as' => 'payment.notify',
+        'uses' => 'App\Payment\Controllers\Index@notify'
     ]);
 
 });
