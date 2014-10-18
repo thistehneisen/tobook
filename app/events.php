fb8cc82650286aed904bab20c0c4e6a12ec4f814
An Cao <kcj.pop.com@gmail.com>
2014-10-18 16:18:41 +0300

    Add listener for bookings when payment cancelled.

--- app/events.php
+++ app/events.php
@@ -1,3 +1,4 @@
 <?php
 Event::listen('payment.process', 'App\Appointment\Listeners\PaymentProcessListener');
 Event::listen('payment.success', 'App\Appointment\Listeners\PaymentSuccessListener');
+Event::listen('payment.cancelled', 'App\Appointment\Listeners\PaymentCancelledListener');
