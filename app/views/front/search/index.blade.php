bedab6b2ce812fcd5c08bcf4918e30c6875f08b9
Quang Hung <hungnq1989@gmail.com>
2014-10-20 23:33:17 +0300

    #149 Remove employee avatar in layout-3, change ->hash to ->user->hash

--- app/views/front/search/index.blade.php
+++ app/views/front/search/index.blade.php
@@ -170,7 +170,7 @@ applyCountdown($('span.countdown'));
                 $slots = [];
                 $count = 0;
             ?>
-            <div class="result-row row" data-id="{{ $item->user->id }}" data-url="{{ route('ajax.showBusiness', ['hash' => $item->hash, 'id' => $item->user->id, 'l' => 3]) }}">
+            <div class="result-row row" data-id="{{ $item->user->id }}" data-url="{{ route('ajax.showBusiness', ['hash' => $item->user->hash, 'id' => $item->user->id, 'l' => 3]) }}">
                 <div class="col-sm-12">
                 <img src="{{ Util::thumbnail($item->image, 260, 130) }}" alt="" class="img-responsive img-rounded">
                 <h5>{{ $item->name }}</h5>
--- app/views/modules/as/embed/layout-3/employees.blade.php
+++ app/views/modules/as/embed/layout-3/employees.blade.php
@@ -1,7 +1,6 @@
 <div class="as-employee">
     <label>
         <input type="radio" name="employee_id" value="-1">
-        <img src="//placehold.it/30x30" alt="{{ trans('common.anyone') }}" class="img-circle img-thumbnail">
         {{ trans('common.anyone') }}
     </label>
 </div>
@@ -9,7 +8,6 @@
 <div class="as-employee">
     <label>
         <input type="radio" name="employee_id" value="{{ $employee->id }}">
-        <img src="//placehold.it/30x30" alt="{{ $employee->name }}" class="img-circle img-thumbnail">
         {{ $employee->name }}
     </label>
 </div>
