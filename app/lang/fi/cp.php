bc4f2b135c8657febfa733c1df995f414cc9a768
varaadigital <mikael@varaa.com>
2014-10-21 14:07:34 +0300

    Create cp.php
    mikael adding language file

---
+++ app/lang/fi/cp.php
@@ -0,0 +1,8 @@
+<?php
+// Consumer Payment
+return [
+    'success'          => 'Maksusi onnistui',
+    'success_notice'   => 'Kiitos.',
+    'cancelled'        => 'Maksusi peruttiin',
+    'cancelled_notice' => 'Poistamme kaikki valitut palvelut ja tuotteet.',
+];
