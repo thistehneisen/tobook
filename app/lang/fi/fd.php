3578fcd65e0751dd386f7daacd4a5b2859279206
varaadigital <mikael@varaa.com>
2014-10-21 13:59:00 +0300

    Create fd.php
    Mikael updating language file

---
+++ app/lang/fi/fd.php
@@ -0,0 +1,59 @@
+<?php
+return [
+    'index' => 'Äkkilähdöt',
+    'expired' => 'Päättyneet',
+    'nav' => [
+        'settings' => 'Asetukset',
+    ],
+    'services' => [
+        'all'                  => 'Kaikki palvelut',
+        'add'                  => 'Lisää uusi palvelu',
+        'edit'                 => 'Muokkaa palvelua',
+        'name'                 => 'Nimi',
+        'price'                => 'Normaali hinta',
+        'quantity'             => 'Määrä',
+        'description'          => 'Kuvaus',
+        'business_category_id' => 'Kategoria'
+    ],
+    'coupons' => [
+        'index'            => 'Tarjous',
+        'all'              => 'Kaikki tarjoukset',
+        'add'              => 'Lisää tarjous',
+        'discounted_price' => 'Alennettu hinta',
+        'valid_date'       => 'Voimassa',
+        'quantity'         => 'Määrä',
+        'service_id'       => 'Palvelu',
+        'sold'             => 'Myytyjen määrä',
+        'total'            => 'Yhteensä',
+        'sold'             => 'Myydyt tarjoukset',
+        'active'           => 'Aktiiviset tarjoukset',
+    ],
+    'flash_deals' => [
+        'index'            => 'Lisää äkkilähtö',
+        'all'              => 'Kaikki äkkilähdöt',
+        'add'              => 'Lisää äkkilähtö',
+        'discounted_price' => 'Alennettu hinta',
+        'quantity'         => 'Määrä per aika',
+        'service_id'       => 'Palvelu',
+        'dates'            => 'Päättyneet',
+        'dates_desc'       => 'Tuplapäivät jätetään huomiotta',
+        'existing_dates'   => 'Luodut päivät',
+        'date'             => 'Päivämäärä',
+        'add_date'         => 'Lisää uusi aika',
+        'sold'             => 'Myydyt äkkilähdöt',
+        'active'           => 'Aktiiviset äkkilähdöt'
+    ],
+    'flash_deal_dates' => [
+        'expire'  => 'Päättymispäivä',
+        'remains' => 'Jäljellä'
+    ],
+    'front' => [
+        'explore' => 'Katso lisää',
+        'business' => 'Tarjoaja',
+        'err' => [
+            'not_found'      => 'Äkkilähtöjä ei ole saatavilla',
+            'not_found_long' => 'Äkkilähtö saattaa olla poistettu tai myyty',
+            'sold_out'       => 'Valitettavasti äkkilähtö on loppuunmyyty.',
+        ]
+    ]
+];
