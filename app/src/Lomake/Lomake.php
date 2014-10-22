thistehneisen/tobook

commit b72c03378fcc4b0efb1541ffe2a0e4febc142169

Ref #152, switch to use Lomake for profile business form

app/src/Lomake/Lomake.php
@@ -91,27 +91,36 @@ class Lomake
         $opt['form']['route'] = $opt['route'];
 
         // Generate fields
+        $fieldsData = [];
         $fields = [];
         if ($opt['overwrite'] === false) {
             foreach ($instance->fillable as $name) {
-                $field['name']       = $name;
-                $field['type']       = $this->guessInputType($name);
-                $field['required']   = $this->isRequired($instance, $name);
-                $field['model']      = $instance;
-                $field['langPrefix'] = $opt['langPrefix'];
+                $fieldData               = [];
+                $fieldData['name']       = $name;
+                $fieldData['type']       = $this->guessInputType($name);
+                $fieldData['required']   = $this->isRequired($instance, $name);
+                $fieldData['model']      = $instance;
+                $fieldData['langPrefix'] = $opt['langPrefix'];
 
-                $fields[$name] = FieldFactory::create($field);
+                $fieldsData[$name]       = $fieldData;
             }
         }
 
-        // If user wants to overwrite generated fields
-        foreach ($opt['fields'] as $name => $field) {
-            $field['name']       = $name;
-            $field['model']      = $instance;
-            $field['langPrefix'] = $opt['langPrefix'];
-            $field['required']   = $this->isRequired($instance, $name);
+        // If user wants to overwrite fields data
+        foreach ($opt['fields'] as $name => $fieldData) {
+            foreach ($fieldData as $fieldDataKey => $fieldDataValue) {
+                $fieldsData[$name][$fieldDataKey] = $fieldDataValue;
+            }
 
-            $fields[$name] = FieldFactory::create($field);
+            if (empty($fieldsData[$name]['type'])) {
+                // allow caller to disable fields from being rendered
+                unset($fieldsData[$name]);
+            }
+        }
+
+        // create fields from data
+        foreach ($fieldsData as $name => $fieldData) {
+            $fields[$name] = FieldFactory::create($fieldData);
         }
 
         // Update the fields list

app/src/Core/Controllers/User.php
@@ -1,6 +1,7 @@
 <?php namespace App\Core\Controllers;
 
 use App\Core\Models\Business;
+use Lomake;
 use Session, Validator, Input, View, Redirect, Hash, Confide;
 use App\Core\Models\User as UserModel;
 use App\Core\Models\BusinessCategory;
@@ -50,6 +51,21 @@ class User extends Base
             $selectedCategories = [];
         }
 
+        if ($user->is_business) {
+            $businessLomake = Lomake::make($business, [
+                'route'             => '#',
+                'langPrefix'        => 'user.business',
+                'fields'            => [
+                    'description'   => ['type' => 'html_field'],
+                    'size'          => ['type' => false],
+                    'lat'           => ['type' => false],
+                    'lng'           => ['type' => false],
+                ],
+            ]);
+        } else {
+            $businessLomake = null;
+        }
+
         if ($user->is_consumer) {
             $consumer = $user->consumer;
         } else {
@@ -65,6 +81,7 @@ class User extends Base
         return View::make('user.profile', [
             'user'               => $user,
             'business'           => $business,
+            'businessLomake'     => $businessLomake,
             'consumer'           => $consumer,
             'fields'             => $fields,
             'validator'          => Validator::make(Input::all(), $this->rules['profile']),

app/src/Lomake/Fields/HtmlField.php
@@ -1,0 +1,12 @@
+<?php namespace App\Lomake\Fields;
+
+class HtmlField extends Text
+{
+    /**
+     * @{@inheritdoc}
+     */
+    public function render()
+    {
+        return call_user_func_array('Form::textarea', $this->getParams());
+    }
+}

app/src/Lomake/views/fields/group.blade.php
@@ -1,0 +1,8 @@
+<div class="form-group {{ Form::errorCSS($field->name, $errors) }}">
+    {{ Form::label($field->name, $field->label, ['class' => 'col-sm-2 col-sm-offset-1 control-label']) }}
+    <div class="col-sm-6">
+    {{ $field }}
+    <!-- Validation error -->
+    {{ Form::errorText($field->name, $errors) }}
+    </div>
+</div>

app/src/Lomake/views/form.blade.php
@@ -1,14 +1,7 @@
 {{ Form::open($opt['form']) }}
     <!-- Fields -->
     @foreach ($fields as $field)
-        <div class="form-group {{ Form::errorCSS($field->name, $errors) }}">
-            {{ Form::label($field->name, $field->label, ['class' => 'col-sm-2 col-sm-offset-1 control-label']) }}
-            <div class="col-sm-6">
-            {{ $field }}
-            <!-- Validation error -->
-            {{ Form::errorText($field->name, $errors) }}
-            </div>
-        </div>
+        @include('varaa-lomake::fields.group')
     @endforeach
 
     <!-- Action buttons -->

app/views/user/el/business.blade.php
@@ -1,15 +1,9 @@
 {{ Form::open(['id' => 'frm-profile', 'route' => 'user.profile', 'class' => 'form-horizontal', 'role' => 'form']) }}
     <h3 class="comfortaa orange">{{ trans('user.profile.business') }}</h3>
 
-@foreach (['name', 'description', 'phone', 'address', 'city', 'postcode', 'country'] as $field)
-    <div class="form-group {{ Form::errorCSS($field, $errors) }}">
-        {{ Form::label($field, trans('user.business.'.$field).Form::required($field, $validator), ['class' => 'col-sm-2 col-sm-offset-1 control-label']) }}
-        <div class="col-sm-6">
-            {{ Form::text($field, Input::get($field, $business->$field), ['class' => 'form-control']) }}
-            {{ Form::errorText($field, $errors) }}
-        </div>
-    </div>
-@endforeach
+    @foreach ($businessLomake->fields as $field)
+        @include('varaa-lomake::fields.group')
+    @endforeach
 
     <div class="form-group {{ Form::errorCSS('size', $errors) }}">
         <label for="size" class="col-sm-2 col-sm-offset-1 control-label">{{ trans('user.business.size') }}</label>

commit fae86b46b1f2f0020be4c313cb20f710117ff31a

Fix bug missing field data for overwritten Lomake fields introduced by commit b72c033

app/src/Lomake/Lomake.php
@@ -115,6 +115,22 @@ class Lomake
 
         // If user wants to overwrite fields data
         foreach ($opt['fields'] as $name => $fieldData) {
+            if (!isset($fieldData['name'])) {
+                $fieldData['name'] = $name;
+            }
+            if (!isset($fieldData['type'])) {
+                $fieldData['type'] = $this->guessInputType($fieldData['name']);
+            }
+            if (!isset($fieldData['required'])) {
+                $fieldData['required'] = $this->isRequired($instance, $fieldData['name']);
+            }
+            if (!isset($fieldData['model'])) {
+                $fieldData['model'] = $instance;
+            }
+            if (!isset($fieldData['langPrefix'])) {
+                $fieldData['langPrefix'] = $opt['langPrefix'];
+            }
+
             foreach ($fieldData as $fieldDataKey => $fieldDataValue) {
                 $fieldsData[$name][$fieldDataKey] = $fieldDataValue;
             }