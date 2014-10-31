f1ab0cf655dfdfdb8822690b9ff9ce413af89ab5
An Cao <kcj.pop.com@gmail.com>
2014-10-31 19:35:47 +0200

    Allow to pass a callbacks as presenter.

--- app/src/Olut/Bartender.php
+++ app/src/Olut/Bartender.php
@@ -26,6 +26,14 @@ class Bartender
 
     public function mix($field, $item)
     {
+        // Let's see if we got a callable object
+        if (!empty($this->presenters[$field])
+            && is_callable($this->presenters[$field])) {
+            return $this->presenters[$field]($item->$field, $item);
+        }
+
+        // If not, maybe we have a class name
+        // Or in the worst case, we need to use the default presenter
         $presenterClass = !empty($this->presenters[$field])
             ? $this->presenters[$field]
             : $this->getDefaultPresenter($field);
