130e7e38a7a63054513a4626f2894bebcdee0a52
Hieu Nguyen <hieuh25@gmail.com>
2014-11-02 18:54:03 +0200

    Add more migration to get all info to businesses table

---
+++ app/database/migrations/2014_11_02_182034_migrate_business.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Core\Models\Business;
+
+class MigrateBusiness extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (User::all() as $user) {
+            if (!empty($user->business->name)) {
+                continue;
+            }
+
+            $input = [
+                'name' => $user->username,
+                'description' => $user->description,
+                'size' => $user->business_size,
+                'address' => $user->address,
+                'city' => $user->city,
+                'postcode' => $user->postcode,
+                'country' => $user->country,
+                'phone' => $user->phone,
+                'lat' => $user->lat,
+                'lng' => $user->lng,
+            ];
+
+            array_walk($input, function (&$value, $key) {
+                if (empty($value)) {
+                    if ($key === 'phone' or $key === 'name') {
+                        $value = 'N/A';
+                    } else {
+                        $value = '';
+                    }
+                }
+            });
+
+            $business = new Business($input);
+            $business->is_activated = true;
+            $business->user()->associate($user);
+            $business->saveOrFail();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+}
