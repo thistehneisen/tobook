4015b013dc19d63e7d7f58623f7339fe967c8d41
An Cao <kcj.pop.com@gmail.com>
2014-11-03 18:50:47 +0200

    Change form layout to edit business in admin.

---
+++ app/views/admin/users/form.blade.php
@@ -0,0 +1,40 @@
+@if ($layout)
+    @extends ($layout)
+@endif
+
+@section ('content')
+    @if (isset($item->id))
+        <h4 class="comfortaa">{{ trans($langPrefix.'.edit') }}</h4>
+    @else
+        <h4 class="comfortaa">{{ trans($langPrefix.'.add') }}</h4>
+    @endif
+
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#tab-general" role="tab" data-toggle="tab">General</a></li>
+    <li role="presentation"><a href="#tab-business" role="tab" data-toggle="tab">Business Information</a></li>
+    <li role="presentation"><a href="#tab-services" role="tab" data-toggle="tab">Active Services</a></li>
+</ul>
+
+<br>
+<!-- Tab panes -->
+<div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="tab-general">
+        {{ $lomake->open() }}
+        {{ $lomake->render() }}
+        {{ $lomake->close() }}
+    </div> <!-- tab general -->
+
+    <div role="tabpanel" class="tab-pane" id="tab-business">
+
+    </div> <!-- tab business -->
+
+    <div role="tabpanel" class="tab-pane" id="tab-services">
+    </div> <!-- tab services -->
+</div>
+
+<div class="modal-form">
+    @include ('el.messages')
+
+</div>
+@stop
