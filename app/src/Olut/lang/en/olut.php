a7e3f867b4e3c01b1c4447df915c74c609cb7b27
An Cao <kcj.pop.com@gmail.com>
2014-11-03 21:36:55 +0200

    Update wording.

--- app/src/Olut/lang/en/olut.php
+++ app/src/Olut/lang/en/olut.php
@@ -4,7 +4,7 @@ return [
     'success_add'    => 'Item was created successfully.',
     'success_edit'   => 'Data was updated successfully.',
     'success_delete' => 'Item was deleted successfully.',
-    'success_bulk'   => 'Item was deleted successfully.',
+    'success_bulk'   => 'Selected items has been updated successfully.',
     'sortable'       => 'Drag to reorder',
     'per_page'       => 'Items per page',
     'with_selected'  => 'With selected',
