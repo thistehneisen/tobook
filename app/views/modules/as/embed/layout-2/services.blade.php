05722aa74d525e98a140a9eff6140cfa5bca97d9
Quang Hung <hungnq1989@gmail.com>
2014-11-05 10:41:03 +0200

    #180 Move service time description to blow the time

--- app/views/modules/as/embed/layout-2/services.blade.php
+++ app/views/modules/as/embed/layout-2/services.blade.php
@@ -15,12 +15,14 @@
         <div class="as-service-row">
             <a data-service-id="{{ $service->id }}" href="#" class="as-service"><small><i class="glyphicon glyphicon-chevron-right"></i></small> {{ $service->name }}</a>
             <div class="as-service-time">
-                <div class="btn-group">
-                    <button data-service-time-id="default" data-service-id="{{ $service->id }}" type="button" class="btn btn-default btn-sm btn-service-time"><i class="glyphicon glyphicon-time"></i> {{ $service->during }} {{ trans('common.minutes') }}</button>
+                <div class="btn-group service-time">
+                    <div class="col-lg-12"><button data-service-time-id="default" data-service-id="{{ $service->id }}" type="button" class="btn btn-default btn-sm btn-service-time"><i class="glyphicon glyphicon-time"></i> {{ $service->during }} {{ trans('common.minutes') }}</button></div>
+                    <div class="visible-xs col-lg-12">&nbsp;</div>
                 </div>
             @foreach ($service->serviceTimes as $item)
-                <div class="btn-group">
-                    <button data-service-id="{{ $service->id }}" data-service-time-id="{{ $item->id }}" type="button" class="btn btn-default btn-sm btn-service-time"><i class="glyphicon glyphicon-time"></i> {{ $item->during }} {{ trans('common.minutes') }}</button> &nbsp; {{ $item->description }}
+                <div class="btn-group service-time">
+                    <div class="col-lg-12"><button data-service-id="{{ $service->id }}" data-service-time-id="{{ $item->id }}" type="button" class="btn btn-default btn-sm btn-service-time"><i class="glyphicon glyphicon-time"></i> {{ $item->during }} {{ trans('common.minutes') }}</button></div>
+                    <div class="col-lg-12">{{ $item->description }}</div>
                 </div>
             @endforeach
             </div>
