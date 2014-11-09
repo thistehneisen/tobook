b1b7af14dc44f56da782a3ccccf1b9a10a8ae6ce
Dao Hoang Son <daohoangson@gmail.com>
2014-11-10 03:01:56 +0700

    Ref #151, add new feature: import CSV of consumers

---
+++ app/views/modules/co/import.blade.php
@@ -0,0 +1,24 @@
+@extends ('modules.co.layout')
+
+@section ('content')
+    @include('modules.co.tabs')
+
+    @include ('el.messages')
+
+{{ Form::open(['route' => ['consumer-hub.doImport'], 'class' => 'form-horizontal well', 'role' => 'form', 'files' => true]) }}
+
+    <div class="form-group {{ Form::errorCSS('upload', $errors) }}">
+        <label for="upload" class="col-sm-2 control-label">{{ trans('co.import.upload_csv') }}</label>
+        <div class="col-sm-5">
+           {{ Form::file('upload', ['id' => 'upload']) }}
+           {{ Form::errorText('upload', $errors) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-5">
+            <button type="submit" class="btn btn-primary" id="btn-import">{{ trans('co.import.import') }}</button>
+        </div>
+    </div>
+{{ Form::close() }}
+@stop
