837c7bfe1b27a188d390d0ddc0e5b264dbf9ae80
Quang Hung <hungnq1989@gmail.com>
2014-11-23 22:10:15 +0200

    #214 Unitest for single employee timetable (custome time and default working time)

--- app/tests/unit/Appointment/Models/Layout3Cest.php
+++ app/tests/unit/Appointment/Models/Layout3Cest.php
@@ -21,9 +21,11 @@ class UnitLayout3Cest
     private $service  = null;
     private $user = null;
 
-    public function testTimetableOfSingle(UnitTester $t)
+    public function testTimetableOfSingleCustom(UnitTester $t)
     {
         $this->initData();
+        $this->initCustomTime();
+
         $layout3 = new Layout3;
         $date = new Carbon('2014-11-24 00:00:00');
         $timeTable = $layout3->getTimetableOfSingle($this->employee, $this->service, $date);
@@ -52,6 +54,54 @@ class UnitLayout3Cest
             '15:30',
             '15:45',
             '16:00',
+        ];
+        $t->assertEquals(array_keys($timeTable), $result);
+    }
+
+    public function testTimetableOfSingleDefault(UnitTester $t)
+    {
+        $this->initData();
+        $layout3 = new Layout3;
+        $date = new Carbon('2014-11-24 00:00:00');
+        $timeTable = $layout3->getTimetableOfSingle($this->employee, $this->service, $date);
+        $result = [
+            '08:00',
+            '08:15',
+            '08:30',
+            '08:45',
+            '09:00',
+            '09:15',
+            '09:30',
+            '09:45',
+            '10:00',
+            '10:15',
+            '10:30',
+            '10:45',
+            '11:00',
+            '11:15',
+            '11:30',
+            '11:45',
+            '12:00',
+            '12:15',
+            '12:30',
+            '12:45',
+            '13:00',
+            '13:15',
+            '13:30',
+            '13:45',
+            '14:00',
+            '14:15',
+            '14:30',
+            '14:45',
+            '15:00',
+            '15:15',
+            '15:30',
+            '15:45',
+            '16:00',
+            '16:15',
+            '16:30',
+            '16:45',
+            '17:00',
         ];
         $t->assertEquals(array_keys($timeTable), $result);
     }
@@ -92,8 +142,11 @@ class UnitLayout3Cest
         $this->service->category()->associate($this->category);
         $this->service->saveOrFail();
         $this->service->employees()->attach($this->employee);
+    }
 
-        //Init custome time
+    public function initCustomTime()
+    {
+         //Init custome time
         $customTime = new CustomTime;
 
         $customTime->fill([
