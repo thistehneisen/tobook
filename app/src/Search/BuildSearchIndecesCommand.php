a2bf28f3ac0d4d40e4a53f55e1e6d22c4f6bf256
An Cao <kcj.pop.com@gmail.com>
2014-12-02 22:27:44 +0200

    Add custom nGram analyzer while building indices.

--- app/src/Search/BuildSearchIndecesCommand.php
+++ app/src/Search/BuildSearchIndecesCommand.php
@@ -1,8 +1,9 @@
 <?php namespace App\Search;
 
-use Queue;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Queue;
+use Elasticsearch\Client;
 
 class BuildSearchIndecesCommand extends Command
 {
@@ -21,12 +22,21 @@ class BuildSearchIndecesCommand extends Command
     protected $description = 'Build indices of one or many models';
 
     /**
+     * ElasticSearch client
+     *
+     * @var ElasticSearch\Client
+     */
+    protected $client;
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function fire()
     {
+        $this->client = new Client();
+
         $default = [
             'App\Core\Models\User',
             'App\Core\Models\Business',
@@ -41,6 +51,7 @@ class BuildSearchIndecesCommand extends Command
 
         foreach ($models as $model) {
             $this->info('Enqueue to rebuild index of '.$model);
+            $this->createIndex($model);
 
             foreach ($model::all() as $item) {
                 // Push into queue to reindex
@@ -54,6 +65,57 @@ class BuildSearchIndecesCommand extends Command
                 });
             }
         }
+    }
+
+    /**
+     * Create index for a model, if not existing
+     *
+     * @param string $model Model name
+     *
+     * @return void
+     */
+    protected function createIndex($model)
+    {
+        $params = [];
+        $params['index'] = $model::getSearchIndexName();
+        // If it exists already, return ASAP
+        if ($this->client->indices()->exists($params)) {
+            return;
+        }
+
+        // Build a custom tokenizer
+        $params['body']['settings']['analysis']['analyzer'] = [
+            'varaa_ngrams' => [
+                'type' => 'custom',
+                'tokenizer' => 'varaa_tokenizer',
+                'filter' => ['lowercase']
+            ]
+        ];
+
+        //Index 'Hakunamatata' => 'Hak','aku','una' .etc
+        $params['body']['settings']['analysis']['tokenizer'] = [
+            'varaa_tokenizer' => [
+                'type'        => 'nGram',
+                'min_gram'    => '4',
+                'max_gram'    => '8',
+                'token_chars' => ['letter', 'digit']
+            ]
+        ];
+
+        // Auto mapping all fillable fields of that model to use
+        $map = ['_source' => ['enabled' => true]];
+
+        // Get all fillable fields
+        $instance = new $model();
+        foreach ($instance->getFillable() as $field) {
+            $map['properties'][$field] = [
+                'type' => 'string',
+                'analyzer' => 'varaa_ngrams'
+            ];
+        }
+        $params['body']['mappings'][$model::getSearchIndexType()] = $map;
+
+        $this->client->indices()->create($params);
     }
 
     /**
