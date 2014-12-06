c359bc90a2ac120adc99275fac5c55a25f530ec2
An Cao <kcj.pop.com@gmail.com>
2014-12-06 10:58:45 +0200

    Verify there's no duplicated data, close #210.

--- app/tests/functional/Admin/UserCest.php
+++ app/tests/functional/Admin/UserCest.php
@@ -10,12 +10,21 @@ class UserCest extends \Test\Functional\Base
 {
     protected $editUrl;
     protected $indexUrl;
+    protected $stub;
 
     public function _before(FunctionalTester $i)
     {
         parent::_before($i);
         $this->editUrl = route('admin.users.upsert', ['id' => $this->user->id]);
         $this->indexUrl = route('admin.users.index');
+        $this->stub = [
+            'note'             => 'Lorem',
+            'name'             => 'Foo',
+            'phone'            => '123 456 789',
+            'meta_title'       => 'Meta title',
+            'meta_keywords'    => 'Meta keywords',
+            'meta_description' => 'Meta description',
+        ];
     }
 
     public function seePageToEditBusiness(FunctionalTester $i)
@@ -26,20 +35,11 @@ class UserCest extends \Test\Functional\Base
 
     public function editBusinessInformation(FunctionalTester $i)
     {
-        $input = [
-            'note'             => 'Lorem',
-            'name'             => 'Foo',
-            'phone'            => '123 456 789',
-            'meta_title'       => 'Meta title',
-            'meta_keywords'    => 'Meta keywords',
-            'meta_description' => 'Meta description',
-        ];
-
         $i->amOnPage($this->editUrl);
-        $i->submitForm('#business-form', $input + ['_token' => csrf_token()]);
+        $i->submitForm('#business-form', $this->stub + ['_token' => csrf_token()]);
 
         $business = Business::find($this->user->business->id);
-        foreach ($input as $field => $value) {
+        foreach ($this->stub as $field => $value) {
             $i->assertEquals($business->$field, $value);
         }
     }
@@ -58,5 +58,14 @@ class UserCest extends \Test\Functional\Base
         $i->seeElement('[name=meta_title]');
         $i->seeElement('[name=meta_keywords]');
         $i->seeElement('[name=meta_description]');
+    }
+
+    public function seeNoDuplicatedBusinessInformation(FunctionalTester $i)
+    {
+        $i->amOnPage($this->editUrl);
+        $i->submitForm('#business-form', $this->stub + ['_token' => csrf_token()]);
+
+        $business = Business::ofUser($this->user->id);
+        $i->assertTrue($business->count() === 1, 'no duplicated business');
     }
 }
