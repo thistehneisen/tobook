c085aa6d035e8de7bf86f4018f0320b1cf6e360a
Hieu Nguyen <hieuh25@gmail.com>
2014-12-09 06:39:36 +0200

    Temporarily disable some logs to keep event count in Sentry low.

--- app/src/Appointment/NAT/Service.php
+++ app/src/Appointment/NAT/Service.php
@@ -49,7 +49,7 @@ class Service
         // For God's sake, we need to build NAT for 4 days in advance
         $i = 0;
         while ($i < 4) {
-            Log::debug('Queue to build NAT', ['userId' => $user->id, 'date' => $date->toDateString()]);
+            //Log::debug('Queue to build NAT', ['userId' => $user->id, 'date' => $date->toDateString()]);
 
             // Push this job into the special queue called 'varaa:nat'
             Queue::push('App\Appointment\NAT\Service@scheduledBuild', [
