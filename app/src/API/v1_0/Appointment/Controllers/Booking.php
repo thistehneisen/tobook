thistehneisen/tobook

commit 48fa9c5c3d1382c21277633f3ea3bcc25c32dd79

Refactor method to produce uuid for AS

app/src/API/v1_0/Appointment/Controllers/Booking.php
@@ -1,8 +1,9 @@
 <?php namespace App\API\v1_0\Appointment\Controllers;
 
 use App\Appointment\Models\Consumer;
-use Confide, Input, Request, Response, Util;
+use Confide, Input, Request, Response;
 use App\Appointment\Models\Employee;
+use App\Appointment\Models\Booking;
 use App\Appointment\Models\BookingExtraService;
 use App\Appointment\Models\BookingService;
 use App\Appointment\Models\AsConsumer;
@@ -219,7 +220,7 @@ class Booking extends Base
                 }
             }
         } else {
-            $uuid = Util::uuid();
+            $uuid = Booking::uuid();
 
             foreach ($extraServices as $extraService) {
                 // all extra services are new

app/src/Appointment/Models/Booking.php
@@ -1,6 +1,6 @@
 <?php namespace App\Appointment\Models;
 
-use Request, Carbon\Carbon, NAT;
+use Request, Carbon\Carbon, NAT, Util;
 use App\Core\Models\User;
 use App\Appointment\Models\Observer\SmsObserver;
 use App\Appointment\Models\ResourceService;
@@ -769,5 +769,20 @@ class Booking extends \App\Appointment\Models\Base implements \SplSubject
         $booking = Booking::find($booking->id);
 
         return self::updateBooking($booking);
+    }
+
+    /**
+     * Generate a random 16-character string for Booking UUID
+     *
+     * @return string
+     */
+    public static function uuid()
+    {
+        while (true) {
+            $uuid = Util::uuid();
+            if (is_null(self::where('uuid', '=', $uuid)->first())) {
+                return $uuid;
+            }
+        }
     }
 }

app/tests/helpers/Traits/Booking.php
@@ -2,7 +2,7 @@
 namespace Test\Traits;
 
 use App\API\v1_0\Appointment\Controllers\Consumer;
-use Config, Util;
+use Config;
 use App\Appointment\Models\AsConsumer;
 use App\Appointment\Models\BookingExtraService;
 use App\Appointment\Models\BookingService;
@@ -18,7 +18,7 @@ trait Booking
             return null;
         }
 
-        $uuid = Util::uuid();
+        $uuid = \App\Appointment\Models\Booking::uuid();
         $service = $category->services()->first();
 
         $employee =  (empty($employee))

app/src/Appointment/Controllers/Bookings.php
@@ -223,7 +223,7 @@ class Bookings extends AsBase
         $categories = $this->getCategories($services);
 
         return View::make('modules.as.bookings.form', [
-            'uuid'            => Util::uuid(),
+            'uuid'            => Booking::uuid(),
             'employee'        => $employee,
             'categories'      => $categories,
             'bookingDate'     => $bookingDate,

app/src/Appointment/Controllers/FrontBookings.php
@@ -42,7 +42,7 @@ class FrontBookings extends Bookings
         $hash            = Input::get('hash');
         $bookingDate     = Input::get('booking_date');
         $startTimeStr    = trim(Input::get('start_time'));
-        $uuid            = Input::get('uuid', Util::uuid());
+        $uuid            = Input::get('uuid', Booking::uuid());
 
 
         //TODO check if is there any potential error

app/src/Core/Util.php
@@ -10,15 +10,17 @@ use Imagine;
 class Util
 {
     /**
-     * Generate a random 36-character string for Booking UUID
+     * Generate a random 16-character string for booking UUID
+     * NOTE: there should be module specific method to wrap around this method
+     * to guarantee each uuid generate is unique
+     * Check example in \App\Appointment\Models\Booking::uuid()
+     *
      * @return string
      */
-    public static function uuid()
+    public static function uuid($length = 16)
     {
-        // Legacy from old source code, 12 char (not so random)
-        //return chr(rand(65,90)) . chr(rand(65,90)) . time();
-
-        return uniqid(str_random(5));
+        // use quickRandom for faster performance
+        return Str::quickRandom($length);
     }
 
     /**

app/tests/unit/Appointment/Models/BookingCest.php
@@ -14,7 +14,7 @@ use App\Appointment\Models\AsConsumer;
 use App\Core\Models\User;
 use Carbon\Carbon;
 use \UnitTester;
-use DB, Util;
+use DB;
 
 /**
  * @group as
@@ -246,7 +246,7 @@ class BookingCest
 
     public function testSaveBookingSuccess(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $user = User::find(70);
         $employee = Employee::find(63);
         $service = Service::find(301);
@@ -310,7 +310,7 @@ class BookingCest
 
     public function testSaveBookingNoServices(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $user = User::find(70);
         $consumer = Consumer::find(1);
         $input = [];
@@ -374,7 +374,7 @@ class BookingCest
 
         $date = with(new \Carbon\Carbon())->year(2014)->month(11)->day(10);
         foreach ([$employee1, $employee2] as $employee) {
-            $uuid = Util::uuid();
+            $uuid = Booking::uuid();
 
             // try only the part can fail
             try {

app/tests/unit/Appointment/Models/BookingServiceCest.php
@@ -1,5 +1,6 @@
 <?php namespace Test\Appointment\Models;
 
+use App\Appointment\Models\Booking;
 use App\Appointment\Models\Employee;
 use App\Appointment\Models\EmployeeFreetime;
 use App\Appointment\Models\ExtraService;
@@ -10,7 +11,6 @@ use App\Appointment\Models\ServiceCategory;
 use App\Appointment\Models\ServiceTime;
 use App\Core\Models\User;
 use \UnitTester;
-use Util;
 
 /**
  * @group as
@@ -45,7 +45,7 @@ class BookingServiceCest
     public function testGetCartEndAt(UnitTester $I)
     {
         $bookingService = new BookingService([
-            'tmp_uuid' => Util::uuid(),
+            'tmp_uuid' => Booking::uuid(),
             'start_at' => '12:00:00',
         ]);
         $bookingService->employee()->associate(Employee::find(63));
@@ -68,7 +68,7 @@ class BookingServiceCest
 
         // service #301 with employee #64 with 15 minute plus time
         $bookingService = new BookingService([
-            'tmp_uuid' => Util::uuid(),
+            'tmp_uuid' => Booking::uuid(),
             'start_at' => '12:00:00',
         ]);
         $bookingService->employee()->associate(Employee::find(64));
@@ -79,7 +79,7 @@ class BookingServiceCest
 
     public function testGetExtraServices(UnitTester $I)
     {
-        $uuid = Util::uuid();
+        $uuid = Booking::uuid();
         $extraService = ExtraService::find(1);
 
         for ($i = 0; $i < 3; $i++) {
@@ -104,7 +104,7 @@ class BookingServiceCest
     public function testGetEmployeePlustime(UnitTester $I)
     {
         $bookingService = new BookingService([
-            'tmp_uuid' => Util::uuid(),
+            'tmp_uuid' => Booking::uuid(),
             'start_at' => '12:00:00',
         ]);
         $bookingService->service()->associate(Service::find(301));
@@ -117,7 +117,7 @@ class BookingServiceCest
         $I->assertEquals(0, $plustime, 'plus time but result cached');
 
         $bookingService = new BookingService([
-            'tmp_uuid' => Util::uuid(),
+            'tmp_uuid' => Booking::uuid(),
             'start_at' => '12:00:00',
         ]);
         $bookingService->service()->associate(Service::find(301));
@@ -172,7 +172,7 @@ class BookingServiceCest
 
     public function testSaveBookingServiceSuccess(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $employee = Employee::find(63);
         $service = Service::find(301);
         $date = $this->_getNextDate();
@@ -199,7 +199,7 @@ class BookingServiceCest
 
     public function testSaveBookingServiceDateTimeFormatting(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $employee = Employee::find(63);
         $service = Service::find(301);
 
@@ -236,7 +236,7 @@ class BookingServiceCest
 
     public function testSaveBookingServiceServiceTime(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $employee = Employee::find(63);
         $service = Service::find(301);
         $date = $this->_getNextDate();
@@ -257,7 +257,7 @@ class BookingServiceCest
 
     public function testSaveBookingServiceModifyTime(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $employee = Employee::find(63);
         $service = Service::find(301);
         $date = $this->_getNextDate();
@@ -278,7 +278,7 @@ class BookingServiceCest
 
     public function testSaveBookingServicePlustime(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $employee = Employee::find(64);
         $service = Service::find(301);
         $date = $this->_getNextDate();
@@ -307,7 +307,7 @@ class BookingServiceCest
         $booking = $this->_book($user, $category, $date, $startTime);
         $I->assertNotEmpty($booking, 'booking');
 
-        $uuid = Util::uuid();
+        $uuid = Booking::uuid();
         $service = $category->services()->first();
 
         // the 1 minute overlap should trigger the exception
@@ -334,7 +334,7 @@ class BookingServiceCest
         $booking = $this->_book($user, $category, $date, $startTime);
         $I->assertNotEmpty($booking, 'booking');
 
-        $uuid = Util::uuid();
+        $uuid = Booking::uuid();
         $service = $category->services()->first();
         $extraService = $service->extraServices()->first();
         $input = [
@@ -366,7 +366,7 @@ class BookingServiceCest
 
     public function testSaveBookingServiceOverlapEmployeeFreetime(UnitTester $I)
     {
-        $uuid = \Util::uuid();
+        $uuid = Booking::uuid();
         $employee = Employee::find(63);
         $service = Service::find(301);
         $date = $this->_getNextDate();

commit 32bd38b5884dc741827a5b8f54d68c89a4cc94e0

Fix bug creating booking via API caused by 48fa9c5 and 8caf73f

app/src/API/v1_0/Appointment/Controllers/Booking.php
@@ -219,7 +219,7 @@ class Booking extends Base
                 }
             }
         } else {
-            $uuid = Booking::uuid();
+            $uuid = BookingModel::uuid();
 
             foreach ($extraServices as $extraService) {
                 // all extra services are new