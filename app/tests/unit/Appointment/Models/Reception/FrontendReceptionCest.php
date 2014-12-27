e5caa2d617177e97192f27f7a45e4b0425d4da96
Quang Hung <hungnq1989@gmail.com>
2014-12-27 04:09:56 +0200

    #226 Same test case with rooms for frontend

--- app/tests/unit/Appointment/Models/Reception/FrontendReceptionCest.php
+++ app/tests/unit/Appointment/Models/Reception/FrontendReceptionCest.php
@@ -116,4 +116,80 @@ class FrontendReceptionCest
         $I->assertEquals($booking->endTime, $date->hour(15)->minute(0)->second(0));
         $I->assertEquals($booking->firstBookingService()->is_requested_employee, true);
     }
+
+    public function testValidateWithRoom(UnitTester $I)
+    {
+        $this->initData(true, false, true);
+        $this->initCustomTime();
+
+        $user      = User::find(70);
+        $employee  = $this->employee;
+        $service   = $this->service;
+        $uuid      = Booking::uuid();
+
+        $date      = $this->getDate();
+        $startTime = '09:00';
+
+        $I->amLoggedAs($user);
+        $I->assertEquals($service->length, 60);
+
+        $receptionist = new FrontendReceptionist();
+        $receptionist->setBookingId(0)
+            ->setUUID($uuid)
+            ->setUser($user)
+            ->setBookingDate($date->toDateString())
+            ->setStartTime($startTime)
+            ->setServiceId($service->id)
+            ->setEmployeeId($employee->id)
+            ->setServiceTimeId('default');
+
+        $receptionist->validateWithRooms();
+
+        $I->assertEquals($receptionist->getRoomId(), 1);
+
+        $receptionist->upsertBookingService();
+
+        $consumer = Consumer::handleConsumer([
+            'first_name' => 'Consumer First',
+            'last_name' => 'Last ' . $this->service->id,
+            'email' => 'consumer_' . $this->service->id . '@varaa.com',
+            'phone' => '1234567890',
+            'hash' => '',
+        ], $user);
+
+        $receptionist = new FrontendReceptionist();
+        $receptionist->setBookingId(null)
+            ->setUUID($uuid)
+            ->setUser($user)
+            ->setStatus('confirmed')
+            ->setNotes('')
+            ->setIsRequestedEmployee(true)
+            ->setConsumer($consumer)
+            ->setClientIP('192.168.1.1')
+            ->setSource('backend');
+
+        $receptionist->setBookingService();
+
+        $booking = $receptionist->upsertBooking();
+
+        $receptionist = new FrontendReceptionist();
+        $receptionist->setBookingId(0)
+            ->setUUID(Booking::uuid())
+            ->setUser($user)
+            ->setBookingDate($date->toDateString())
+            ->setStartTime($startTime)
+            ->setServiceId($service->id)
+            ->setEmployeeId(64)
+            ->setServiceTimeId('default');
+
+        $exception = array();
+        try {
+           $receptionist->validateWithRooms();
+        } catch(\Exception $ex) {
+            $exception[] = $ex->getMessage();
+        }
+
+        $I->assertNotEmpty($exception[0]);
+        $I->assertEquals($exception[0], trans('as.bookings.error.not_enough_rooms'));
+    }
 }
