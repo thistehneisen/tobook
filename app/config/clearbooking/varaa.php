57cd0a7c66f4a10a90a80505bf48aeae39223477
Hieu Nguyen <hieuh25@gmail.com>
2015-01-04 23:36:59 +0200

    Only allow 3 modules for clearbooking for now

---
+++ app/config/clearbooking/varaa.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'languages' => ['en', 'fi'],
+    'premium_modules' => [
+        'appointment' => [
+            'route_name' => 'as.index',
+            'enable' => true,
+        ],
+        'flashdeal' => [
+            'route_name' => 'fd.index',
+            'enable' => true,
+        ],
+        'consumers' => [
+            'route_name' => 'consumer-hub.index',
+            'enable' => true,
+        ]
+    ]
+];
