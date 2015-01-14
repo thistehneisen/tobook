0f1d07ab6b09b229097e8b9826107032a2b4d86e
Hieu Nguyen <hieuh25@gmail.com>
2015-01-14 03:23:01 +0200

    Add sv translation file

---
+++ app/lang/sv/home.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'customer_websites'     => 'Kundhemsidor',
+    'description'           => 'Create a stunning looking responsive websites!',
+    'copyright_policy'      => 'Copyright Policy',
+    'copyright'             => 'Copyright',
+    'newsletter'            => 'Nyhetsbrev',
+    'enter_your_email'      => 'Fyll i din e-postadress',
+    'submit'                => 'Skicka',
+    'location'              => 'Ort',
+    'telephone'             => 'Telefon',
+    'homepages'             => 'Hemsidor',
+    'loyaltycard'           => 'Loyalty card',
+    'customer_register'     => 'Kundbas',
+    'cashier'               => 'Cashier',
+    'description_1'         => 'Our passion is to create high quality websites which our customers can be proud of',
+    'description_2'         => 'See the company\'s home page layout, and you see the shop and office space with elegance and comfort',
+    'start_now'             => 'Testa gratis!',
+    'tagline'               => 'Everything you need for<br>easy and profitable business',
+    'next_timeslot'         => 'Nästa lediga tider',
+    'time'                  => 'Tid',
+
+    'search_tagline'        => 'Enklare tidsbokning. När du vill. Var du vill.',
+    'search_query'          => 'Sök efter en tjänst',
+    'search_place'          => 'Halmstad, Sverige',
+    'search'        => [
+        'tagline'         => 'Enklare tidsbokning. När du vill. Var du vill.',
+        'query'           => 'Sök tjänst',
+        'location'        => 'Var?',
+        'about'           => 'Om tjänsten',
+        'locations_hours' => 'Adress &amp; Öppettider',
+        'business_hours'  => 'Öppettider',
+        'buy'             => 'Köp',
+        'book'            => 'Boka',
+    ],
+    'cart' => [
+        'add'         => 'Lägg i varukorgen',
+        'items'       => 'produkt|produkter',
+        'empty'       => 'Tom',
+        'empty_long'  => 'Varukorgen är tom.',
+        'checkout'    => 'Kassa',
+        'total'       => 'Totalt',
+        'heading'     => 'Valda produkter',
+        'why_heading' => 'Varför ska jag registrera mig innan jag går till kassan?',
+        'why_content' => 'Som registrerad användare kan du enkelt se och hantera dina bokningar.',
+        'process'     => 'Gå till kassan',
+        'err'         => [
+            'business' => 'Den här tjänsten är tyvärr inte tillåten för företag. Vänligen logga in med ditt personkonto.',
+            'zero_amount' => 'Betalning misslyckades, då det totala värdet är 0 kr.',
+        ]
+    ]
+];
