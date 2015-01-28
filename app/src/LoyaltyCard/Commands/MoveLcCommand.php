a9a9b264e79be6eac6cebbf1b53163bb46059175
An Cao <kcj.pop.com@gmail.com>
2015-01-28 22:52:41 +0200

    Retrieve real email of moving user if existing.

--- app/src/LoyaltyCard/Commands/MoveLcCommand.php
+++ app/src/LoyaltyCard/Commands/MoveLcCommand.php
@@ -61,27 +61,39 @@ class MoveLcCommand extends Command
             1001381 => 'vanhakassu',
         ];
 
+        // Database handler to interact with old WP db
+        $db = DB::connection('old');
+
         // Get role User first, so that we don't need to hit the database again
         $role = Role::user();
         foreach ($users as $oldId => $username) {
+            // Default email
+            // We will try to get the real email soon
+            $email = $username.'@customers.varaa.com';
+
+            // Go to wp_users to fetch email of this user
+            $user = $db->table('users')->where('ID', $oldId)->first();
+            if ($user && !empty($user->user_email)) {
+                $email = $user->user_email;
+            }
 
             // Check if there's existing business with the given username
             // Because users could login by using bot username and email, need
             // to check those two fields
             $user = User::where(DB::raw('LOWER(username)'), $username)
-                ->orWhere(DB::raw('LOWER(email)'), $username.'@varaa.com')
+                ->orWhere(DB::raw('LOWER(email)'), $email)
                 ->first();
 
             // If not, create a new one
             if ($user === null) {
-                $this->info('Creating new user as: '.$username);
+                $this->info('Creating new user as: '.$email);
 
                 $user = new User();
-                $user->email                 = $username.'@varaa.com';
+                $user->email                 = $email;
                 $user->password              = $username.'2mfPdEiy';
                 $user->password_confirmation = $username.'2mfPdEiy';
                 if (!$user->save()) {
-                    $this->error('Cannot create user as: '.$username);
+                    $this->error('Cannot create user as: '.$email);
                     continue;
                 }
                 $user->attachRole($role);
@@ -96,13 +108,14 @@ class MoveLcCommand extends Command
                 $business->user()->associate($user);
                 $business->save();
 
-                // Then push into the queue to migrate their data
-                Queue::push('\App\LoyaltyCard\OldDataMover', [
-                    $username,
-                    $oldId,
-                    $user->id, // New user ID
-                ]);
             }
+
+            // Then push into the queue to migrate their data
+            Queue::push('\App\LoyaltyCard\OldDataMover', [
+                $username,
+                $oldId,
+                $user->id, // User ID in the new system
+            ]);
         }
     }
 }
