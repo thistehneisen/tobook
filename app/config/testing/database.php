dae8194e9ab4745f73f4e37170a8e0f7c7ec4dc7
An Cao <kcj.pop.com@gmail.com>
2015-02-18 16:09:50 +0000

    Read codeception.yml for db username and password

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -1,5 +1,9 @@
 <?php
 
+// Read CodeCeption configuration
+$yml = \Symfony\Component\Yaml\Yaml::parse(base_path('codeception.yml'));
+$db = $yml['modules']['config']['Db'];
+
 return [
     'fetch' => PDO::FETCH_CLASS,
     'default' => 'mysql',
@@ -8,8 +12,8 @@ return [
             'driver'    => 'mysql',
             'host'      => 'localhost',
             'database'  => 'varaa_test',
-            'username'  => 'varaa_test',
-            'password'  => 'varaa_test',
+            'username'  => $db['user'],
+            'password'  => $db['password'],
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => 'varaa_',
