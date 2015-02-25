f1e3b30138ff2f2256315b2825e5f5405797c17d
Hieu Nguyen <hieuh25@gmail.com>
2015-02-26 00:59:54 +0700

    Blah update some text

--- app/views/el/search/front.blade.php
+++ app/views/el/search/front.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row text-left">
             <ul class="list-inline front-nav">
-              <li><a href="#">Izvēlies sfēru</a></li>
+              <li><a href="#">Browse businesses</a></li>
               <li><a href="#">How does it work?</a></li>
               <li class="pull-right"><a href="#">Businesses</a></li>
             </ul>
