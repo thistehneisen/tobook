7c73f9980da0b40a683997513bbf5d36b6351f53
varaadigital <mikael@varaa.com>
2015-03-17 00:20:47 +0200

    Update home.php
    Mikael language update

--- app/lang/fi/home.php
+++ app/lang/fi/home.php
@@ -19,21 +19,21 @@ return [
     'tagline'               => 'Kaikki mitä tarvitset helppoon<br>ja kannattavaan liiketoimintaan',
     'next_timeslot'         => 'Seuraavat vapaat ajat',
     'time'                  => 'Aika',
-    'search_tagline'        => 'What do you want to book?', // @todo
-    'search_query'          => 'Enter business name or service', // @todo
+    'search_tagline'        => 'Mitä haluat varata?', // @todo
+    'search_query'          => 'Syötä yrityksen tai palvelun nimi', // @todo
     'search_place'          => 'Helsinki, FI', // @todo
     'search'        => [
-        'tagline'         => 'What do you want to book?', // @todo
-        'query'           => 'Enter business name or service', // @todo
+        'tagline'         => 'Mitä haluat varata?', // @todo
+        'query'           => 'Syötä yrityksen tai palvelun nimi', // @todo
         'location'        => Settings::get('default_location'), // @todo
         'about'           => 'Esittely',
         'locations_hours' => 'Sijainti &amp; Aukioloajat',
         'business_hours'  => 'Aukioloajat',
         'buy'             => 'Osta',
         'book'            => 'Varaa',
-        'button'          => 'Search', // @todo
-        'date'            => 'Any date', // @todo
-        'time'            => 'Any time', // @todo
+        'button'          => 'Hae', // @todo
+        'date'            => 'Valitse päivä', // @todo
+        'time'            => 'Valitse aika', // @todo
         'geo'             => [
             'info' => 'We will ask for your current location to display results that are close to you.'
         ]
@@ -54,14 +54,14 @@ return [
             'zero_amount' => 'Maksua ei voida suorittaa',
         ]
     ],
-    'choose_category'     => 'Choose category', // @todo
-    'how_does_it_work'    => 'How does it work?', // @todo
-    'businesses'          => 'All businesses', // @todo
-    'businesses_category' => 'Businesses of <strong>:category</strong>', // @todo
-    'more'                => 'More', // @todo
-    'less'                => 'Less', // @todo
-    'companies_offers'    => 'Companies with offers', // @todo
-    'categories'          => 'Categories', // @todo
-    'best_offers'         => 'Best offers', // @todo
-    'no_offers'           => 'There is no offer available.', // @todo
+    'choose_category'     => 'Valitse kategoria', // @todo
+    'how_does_it_work'    => 'Kuinka se toimii?', // @todo
+    'businesses'          => 'Kaikki yritykset', // @todo
+    'businesses_category' => 'Yrityksen joiden <strong>:kategoria</strong>', // @todo
+    'more'                => 'Lisää', // @todo
+    'less'                => 'Vähemmän', // @todo
+    'companies_offers'    => 'Vain yritykset joilla on tarjouksia', // @todo
+    'categories'          => 'Kategoriat', // @todo
+    'best_offers'         => 'Parhaat tarjoukset', // @todo
+    'no_offers'           => 'Tarjouksia ei ole saatavilla.', // @todo
 ];
