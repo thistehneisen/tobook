70c1754a42cf6235a3f9398108699444de09a018
Quang Hung <hungnq1989@gmail.com>
2015-03-17 02:22:38 +0200

    #427 again, modify resources/tobook/as/scripts/layout-3.js

--- resources/varaa/as/scripts/layout-3.js
+++ resources/varaa/as/scripts/layout-3.js
@@ -102,8 +102,9 @@
                 // Assign serviceId to dataStorage
                 dataStorage.serviceId = $this.val();
                 dataStorage.serviceTimeId = $this.data('service-time-id');
-                dataStorage.hash = $this.data('hash');
-
+                if(!dataStorage.hash) {
+                    dataStorage.hash = $this.data('hash');
+                }
                 if(settings.isAutoSelectEmployee || ($('#auto-select-employee').val() === 'true')){
                     $step3.collapse('show');
                     $title3.addClass('collapsable');
