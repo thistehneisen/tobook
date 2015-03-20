5176a2c76fa28efca16e6d8b9d15e72e3eb0264d
An Cao <kcj.pop.com@gmail.com>
2015-03-20 11:16:00 +0200

    Add validation while submitting contact form.

--- app/views/front/el/contact.blade.php
+++ app/views/front/el/contact.blade.php
@@ -1,26 +1,36 @@
 <h3 class="sub-heading">{{ trans('home.business.contact.heading') }}</h3>
 
 {{ Form::open(['route' => ['business.contact', $business->user_id, $business->slug], 'class' => 'form-vertical']) }}
-    <div class="form-group">
+
+    <div class="form-group {{ Form::errorCSS('name', $errors) }}">
         {{ Form::label('name', trans('home.business.contact.name')) }}
         {{ Form::text('name', Input::get('name'), ['class' => 'form-control']) }}
+        {{ Form::errorText('name', $errors) }}
     </div>
-    <div class="form-group">
+
+    <div class="form-group {{ Form::errorCSS('email', $errors) }}">
         {{ Form::label('email', trans('home.business.contact.email')) }}
         {{ Form::text('email', Input::get('email'), ['class' => 'form-control']) }}
+        {{ Form::errorText('email', $errors) }}
     </div>
-    <div class="form-group">
+
+    <div class="form-group {{ Form::errorCSS('phone', $errors) }}">
         {{ Form::label('phone', trans('home.business.contact.phone')) }}
         {{ Form::text('phone', Input::get('phone'), ['class' => 'form-control']) }}
+        {{ Form::errorText('phone', $errors) }}
     </div>
-    <div class="form-group">
+
+    <div class="form-group {{ Form::errorCSS('captcha', $errors) }}">
         {{ Form::label('captcha', trans('home.business.contact.captcha')) }}
         <p><img src="{{ Captcha::img() }}" alt=""></p>
         {{ Form::text('captcha', '', ['class' => 'form-control']) }}
+        {{ Form::errorText('captcha', $errors) }}
     </div>
-    <div class="form-group">
+
+    <div class="form-group {{ Form::errorCSS('message', $errors) }}">
         {{ Form::label('message', trans('home.business.contact.message')) }}
         {{ Form::textarea('message', Input::get('message'), ['class' => 'form-control']) }}
+        {{ Form::errorText('message', $errors) }}
     </div>
 
     <div class="form-group">
