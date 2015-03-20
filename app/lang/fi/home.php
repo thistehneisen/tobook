7b6899895a52978b79379001adb96bad454c453a
An Cao <kcj.pop.com@gmail.com>
2015-03-20 15:03:31 +0200

    Fix mistranslated language keys.

--- app/lang/fi/home.php
+++ app/lang/fi/home.php
@@ -19,21 +19,21 @@ return [
     'tagline'               => 'Kaikki mitä tarvitset helppoon<br>ja kannattavaan liiketoimintaan',
     'next_timeslot'         => 'Seuraavat vapaat ajat',
     'time'                  => 'Aika',
-    'search_tagline'        => 'Mitä haluat varata?', // @todo
-    'search_query'          => 'Syötä yrityksen tai palvelun nimi', // @todo
-    'search_place'          => 'Helsinki, FI', // @todo
+    'search_tagline'        => 'Mitä haluat varata?',
+    'search_query'          => 'Syötä yrityksen tai palvelun nimi',
+    'search_place'          => 'Helsinki, FI',
     'search'        => [
-        'tagline'         => 'Mitä haluat varata?', // @todo
-        'query'           => 'Syötä yrityksen tai palvelun nimi', // @todo
-        'location'        => Settings::get('default_location'), // @todo
+        'tagline'         => 'Mitä haluat varata?',
+        'query'           => 'Syötä yrityksen tai palvelun nimi',
+        'location'        => Settings::get('default_location'),
         'about'           => 'Esittely',
         'locations_hours' => 'Sijainti &amp; Aukioloajat',
         'business_hours'  => 'Aukioloajat',
         'buy'             => 'Osta',
         'book'            => 'Varaa',
-        'button'          => 'Hae', // @todo
-        'date'            => 'Valitse päivä', // @todo
-        'time'            => 'Valitse aika', // @todo
+        'button'          => 'Hae',
+        'date'            => 'Valitse päivä',
+        'time'            => 'Valitse aika',
         'geo'             => [
             'info' => 'We will ask for your current location to display results that are close to you.'
         ]
@@ -54,14 +54,14 @@ return [
             'zero_amount' => 'Maksua ei voida suorittaa',
         ]
     ],
-    'choose_category'     => 'Valitse kategoria', // @todo
-    'how_does_it_work'    => 'Kuinka se toimii?', // @todo
-    'businesses'          => 'Kaikki yritykset', // @todo
-    'businesses_category' => 'Yrityksen joiden <strong>:kategoria</strong>', // @todo
-    'more'                => 'Lisää', // @todo
-    'less'                => 'Vähemmän', // @todo
-    'companies_offers'    => 'Vain yritykset joilla on tarjouksia', // @todo
-    'categories'          => 'Kategoriat', // @todo
-    'best_offers'         => 'Parhaat tarjoukset', // @todo
-    'no_offers'           => 'Tarjouksia ei ole saatavilla.', // @todo
+    'choose_category'     => 'Valitse kategoria',
+    'how_does_it_work'    => 'Kuinka se toimii?',
+    'businesses'          => 'Kaikki yritykset',
+    'businesses_category' => 'Yrityksen joiden <strong>:category</strong>',
+    'more'                => 'Lisää',
+    'less'                => 'Vähemmän',
+    'companies_offers'    => 'Vain yritykset joilla on tarjouksia',
+    'categories'          => 'Kategoriat',
+    'best_offers'         => 'Parhaat tarjoukset',
+    'no_offers'           => 'Tarjouksia ei ole saatavilla.',
 ];
