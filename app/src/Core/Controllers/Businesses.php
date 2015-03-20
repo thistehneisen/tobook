55428c00c5386949b9ba11b100cb2351a351580e
An Cao <kcj.pop.com@gmail.com>
2015-03-20 11:16:00 +0200

    Add validation while submitting contact form.

--- app/src/Core/Controllers/Businesses.php
+++ app/src/Core/Controllers/Businesses.php
@@ -1,8 +1,21 @@
 <?php namespace App\Core\Controllers;
 
+use Validator, Redirect, Input;
+
 class Businesses extends Front
 {
-    public function contact()
+    public function contact($id, $slug)
     {
+        $v = Validator::make(Input::all(), [
+            'name'    => 'required',
+            'email'   => 'required|email',
+            'message' => 'required',
+            'captcha' => 'required|captcha',
+        ]);
+
+        if ($v->fails()) {
+            return Redirect::route('business.index', ['id' => $id, 'slug' => $slug])
+                ->withErrors($v);
+        }
     }
 }
