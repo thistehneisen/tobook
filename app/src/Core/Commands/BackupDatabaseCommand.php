63b6fc362e8309dc9d8a3b0aa3b5b4ddcbd17a12
Quang Hung <hungnq1989@gmail.com>
2015-03-21 19:41:27 +0200

    #432 Command to backup database and push backup file to backup server

--- app/src/Core/Commands/BackupDatabaseCommand.php
+++ app/src/Core/Commands/BackupDatabaseCommand.php
@@ -3,6 +3,7 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Config;
+
 class BackupDatabaseCommand extends Command
 {
     /**
@@ -29,11 +30,18 @@ class BackupDatabaseCommand extends Command
         $db_name = Config::get('database.connections.mysql.database');
         $db_user = escapeshellarg(Config::get('database.connections.mysql.username'));
         $db_pwd  = escapeshellarg(Config::get('database.connections.mysql.password'));
-        $backup_name =  escapeshellarg(sprintf('%s-%s.sql', $db_name, date('Ymd.His')));
-        $command = sprintf("/usr/bin/mysqldump -u%s -p%s %s > %s", $db_user, $db_pwd, $db_name, $backup_name);
-        exec($command);
-
-
+        $backup_name = sprintf('%s-%s.sql.gz', $db_name, date('Ymd.His'));
+        //Dump and compress data to a gzip file
+        $backup_command = sprintf("/usr/bin/mysqldump -u%s -p%s %s | gzip > %s", $db_user, $db_pwd, $db_name, $backup_name);
+        exec($backup_command);
+        //Copy backup file to backup server
+        $copy_command = sprintf('scp %s root@46.101.51.135:/srv/backup', $backup_name);
+        exec($copy_command);
+        //Delete backup file on local server
+        $backup_file = realpath('./' . $backup_name);
+        if(file_exists($backup_file)) {
+            unlink($backup_file);
+        }
     }
 
     /**
