336259156f610477bbddeb6ee1ac80ddd2971e49
An Cao <kcj.pop.com@gmail.com>
2015-03-24 22:32:44 +0200

    Do not show disable modules, #407

--- app/views/admin/users/modules.blade.php
+++ app/views/admin/users/modules.blade.php
@@ -1,10 +1,12 @@
 {{ Form::open(['route' => ['admin.users.modules', $user->id]]) }}
 @foreach ($modules as $name => $value)
+    @if ($value['enable'])
     <div class="form-group checkbox">
         <label>
             {{ Form::checkbox('modules[]', $name, $user->hasModule($name)) }} {{ trans('dashboard.'.$name) }}
         </label>
     </div>
+    @endif
 @endforeach
 
     <div class="form-group">
