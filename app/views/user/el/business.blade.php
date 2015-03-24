9f84d385f4f4b4a3e2a1629c84271d39a82c9648
An Cao <kcj.pop.com@gmail.com>
2015-03-24 07:56:07 +0200

    Change location of preview button

--- app/views/user/el/business.blade.php
+++ app/views/user/el/business.blade.php
@@ -53,8 +53,8 @@
     <div class="form-group">
         <div class="col-sm-6 col-sm-offset-3">
             <input type="hidden" name="tab" value="business">
-            <a target="_blank" href="{{ $business->business_url }}" class="btn btn-default btn-lg comfortaa">{{ trans('user.business.preview') }}</a>
-            <button type="submit" class="btn btn-lg btn-orange to-upper comfortaa pull-right">{{ trans('common.save') }}</button>
+            <button type="submit" class="btn btn-lg btn-orange to-upper comfortaa">{{ trans('common.save') }}</button>
+            <a target="_blank" href="{{ $business->business_url }}" class="btn btn-default btn-lg pull-right">{{ trans('user.business.preview') }}</a>
         </div>
     </div>
 {{ $businessLomake->close() }}
