bf256a5d0a204ceab0d31239a5cef4115e9d3d7c
Quang Hung <hungnq1989@gmail.com>
2015-03-29 02:20:37 +0200

    #438 Add discount_percentage field to as_flash_deals

---
+++ app/database/migrations/2015_03_29_021824_add_discount_percentage_to_as_flash_deals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDiscountPercentageToAsFlashDealsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('as_flash_deals', function(Blueprint $table)
+        {
+            $table->boolean('discount_percentage')->after('discounted_price');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('as_flash_deals', function (Blueprint $table) {
+            $table->dropColumn('discount_percentage');
+        });
+	}
+
+}
