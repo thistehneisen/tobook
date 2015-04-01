5eb526275ccc42cdc228fb589432741d40ffce48
Quang Hung <hungnq1989@gmail.com>
2015-04-01 15:03:37 +0300

    Add varaa_multilanguage table and model

---
+++ app/src/Core/Models/Multilanguage.php
@@ -0,0 +1,30 @@
+<?php namespace App\Core\Models;
+
+use App\Core\Models\User;
+
+class Multilanguage extends Base
+{
+    public $fillable = [
+        'context',
+        'lang',
+        'key',
+        'value'
+    ];
+
+    public $rulesets = [
+        'saving' => [
+            'context' => 'required',
+            'lang' => 'required',
+            'key' => 'required',
+        ]
+    ];
+
+    //--------------------------------------------------------------------------
+    // RELATIONSHIPS
+    //--------------------------------------------------------------------------
+    public function user()
+    {
+        return $this->belongsTo('App\Core\Models\User');
+    }
+
+}
