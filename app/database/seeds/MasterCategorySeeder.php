1357b88620afcdc47b3b12cbf7dbbb337dd485fa
An Cao <kcj.pop.com@gmail.com>
2015-04-04 09:30:33 +0300

    Add seeder for master categories & treatment types

---
+++ app/database/seeds/MasterCategorySeeder.php
@@ -0,0 +1,110 @@
+<?php
+use App\Appointment\Models\MasterCategory;
+use App\Appointment\Models\TreatmentType;
+
+class MasterCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'master' => 'Kampaamopalvelut',
+                'treatment' => [
+                    'Parturipalvelut',
+                    'Värjäykset & raidat',
+                    'Hiustenpidennykset',
+                    'Hää- ja juhlakampaukset',
+                ]
+            ],
+            [
+                'master' => 'Karvanpoistot',
+                'treatment' => [
+                    'Vahaukset',
+                    'Sokeroinnit',
+                    'Laser-karvanpoistot',
+                    'Parranajot',
+                ]
+            ],
+            [
+                'master' => 'Hieronnat',
+                'treatment' => [
+                'Klassiset hieronnat',
+                'Urheiluhieronnat',
+                'Thai-hieronnat',
+                'Kuumakivihieronnat',
+                'Intialaiset päähieronnat',
+                ]
+            ],
+            [
+                'master' => 'Jalkahoidot',
+                'treatment' => [
+                ]
+            ],
+            [
+                'master' => 'Kasvohoidot',
+                'treatment' => [
+                    'Kasvohoito',
+                    'Ihonpuhdistus',
+                    'Terapiahoidot',
+                ]
+            ],
+            [
+                'master' => 'Vartalohoidot',
+                'treatment' => [
+                    'Laihdutushoito',
+                    'Ihonhoito',
+                    'Hemmotteluhoito',
+                ]
+            ],
+            [
+                'master' => 'Kynnet',
+                'treatment' => [
+                    'Manikyyrit',
+                    'Pedikyyrit',
+                    'Rakennekynnet',
+                    'Geelikynnet',
+                ]
+            ],
+            [
+                'master' => 'Ripset & kulmat',
+                'treatment' => [
+                    'Ripsienpidennykset',
+                    'Ripsienpidennysten huolto',
+                    'Kulmien värjäykset ja muotoilut',
+                ]
+            ],
+        ];
+
+        if (MasterCategory::get()->isEmpty() === false) {
+            $this->command->info('MasterCategorySeeder is already seeded');
+
+            return;
+        }
+
+        $order = 1;
+        foreach ($data as $item) {
+            // Create new MasterCategory
+            $category = new MasterCategory([
+                'name' => $item['master'],
+                'order' => $order++
+            ]);
+            $category->save();
+            $category->saveMultilanguage(['fi' => $item['master']], ['fi' => '']);
+
+            // Save TreatmentType
+            foreach ($item['treatment'] as $name) {
+                $treatment = new TreatmentType([
+                    'name' => $name
+                ]);
+                $treatment->masterCategory()->associate($category);
+                $treatment->save();
+                $treatment->saveMultilanguage(['fi' => $name], ['fi' => '']);
+            }
+        }
+    }
+}
