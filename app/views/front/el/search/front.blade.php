20f9b5bbc95e0e5cf80d32de09699038a086efaf
An Cao <kcj.pop.com@gmail.com>
2015-04-06 16:39:34 +0300

    Change background from transparent to white.

--- app/views/front/el/search/front.blade.php
+++ app/views/front/el/search/front.blade.php
@@ -12,7 +12,7 @@
                         </a>
 
                     @if ($category->treatments->isEmpty() === false)
-                        <ul class="dropdown-menu" @if(!empty($category->background_image)) style="background: transparent url({{ $category->background_image }}) center center no-repeat; background-size: cover;" @endif>
+                        <ul class="dropdown-menu" @if(!empty($category->background_image)) style="background: #fff url({{ $category->background_image }}) center center no-repeat; background-size: cover;" @endif>
                         @foreach ($category->treatments as $treatment)
                             <li><a href="#">{{ $treatment->name }}</a></li>
                         @endforeach
