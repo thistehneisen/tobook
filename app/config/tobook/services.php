a5e707eca34e468e72cdd575b63847cb7a166455
Quang Hung <hungnq1989@gmail.com>
2015-04-07 23:27:11 +0300

    Add new oneapi credentical for tobook instance

--- app/config/tobook/services.php
+++ app/config/tobook/services.php
@@ -4,5 +4,10 @@ return [
         'id'           => 65111,
         'password'     => '5f9ec9a8660a545bcba20ac5e2c2004a',
         'verification' => '7f478d2f4a92094f42b2047ed3a37c48',
-    ]
+    ],
+
+    'oneapi' => [
+        'username' => 'Delfi1',
+        'password' => 'D173485i'
+    ],
 ];
