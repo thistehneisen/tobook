0174d74f1ea8156b28ea7087d94cbb6b418c8cad
An Cao <kcj.pop.com@gmail.com>
2015-04-14 00:39:57 +0300

    Modify for better responsiveness.

--- app/views/front/home.blade.php
+++ app/views/front/home.blade.php
@@ -36,6 +36,7 @@
 
 <div class="container">
 
+<div class="hidden-xs hidden-sm">
     <div class="row">
         <div class="col-sm-12">
             <h2 class="text-center orange comfortaa">{{ trans('home.hiw.heading') }}</h2>
@@ -56,6 +57,7 @@
             <p>{{ trans('home.hiw.steps.3_text') }}</p>
         </div>
     </div>
+</div>
 
 @if (App::environment() !== 'tobook')
     <div class="row">
