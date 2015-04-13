2fc1a25fc310aa5d063a764be861e7fda6662f7d
Antti Rössi <antti.rossi@varaa.com>
2015-04-13 23:36:38 +0300

    Update home.php

--- app/lang/ru/home.php
+++ app/lang/ru/home.php
@@ -44,7 +44,7 @@ return [
     'search_place'          => 'Helsinki, FI', // @todo
     'search'        => [
         'tagline'         => 'Что вы хотите заказать?', // @todo
-        'query'           => 'Введите компанию', // @todo
+        'query'           => 'В ведите компанию', // @todo
         'location'        => Settings::get('default_location'), // @todo
         'about'           => 'Подробнее',
         'locations_hours' => 'Места &amp; время',
