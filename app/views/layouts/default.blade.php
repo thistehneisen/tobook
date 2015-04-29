b93697c1abb2599ff91bd101c9f9ce455f4cd34d
An Cao <kcj.pop.com@gmail.com>
2015-04-29 09:40:41 +0300

    Install Zopim on prod.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -34,6 +34,18 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    @if (Request::is('embed/*') === false && App::environment() === 'prod')
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?1b7hTzZJGpnsLXf2RNKlDaznUTHizLMr";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zopim Live Chat Script-->
+    @endif
+
     {{ Settings::get('head_script') }}
 
     {{ Lomake::renderHead() }}
