a1bf93dd797a50cb04883581eceb7ac1f9f098f1
Quang Hung <hungnq1989@gmail.com>
2015-05-10 13:06:27 +0300

    Update paysera info #491

--- app/config/tobook/services.php
+++ app/config/tobook/services.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'paysera' => [
-        'id'           => 65111,
-        'password'     => '5f9ec9a8660a545bcba20ac5e2c2004a',
-        'verification' => '7f478d2f4a92094f42b2047ed3a37c48',
+        'id'           => 63548,
+        'password'     => 'e6e9d37a0f6a79c25564cade197a8e3c',
+        'verification' => '0289ab821b7c434c3c505e31b150cff4',
         'test'         => 1,
     ],
 
