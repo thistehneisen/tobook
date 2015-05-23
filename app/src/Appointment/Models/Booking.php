cb6a13f378945fda38ac03c4f9a3c9701058d54a
Quang Hung <hungnq1989@gmail.com>
2015-05-24 01:59:01 +0300

    Fix join wrong table #507

--- app/src/Appointment/Models/Booking.php
+++ app/src/Appointment/Models/Booking.php
@@ -1045,8 +1045,8 @@ class Booking extends \App\Appointment\Models\Base implements \SplSubject
     {
         $query = static::getCommissionQuery($userId, $status, $employeeId, $start, $end);
 
-        $query = $query->join('as_employees', 'as_employees.id', '=','business_commissions.employee_id')
-            ->join('business_commissions', 'business_commissions.booking_id', '=', 'as_bookings.id')
+        $query = $query->join('business_commissions', 'business_commissions.booking_id', '=', 'as_bookings.id')
+            ->join('as_employees', 'as_employees.id', '=','business_commissions.employee_id')
             ->leftJoin('consumers', 'consumers.id', '=', 'as_bookings.consumer_id')
             ->select(['as_bookings.*', 'as_bookings.id as booking_id', 'as_bookings.date', 'as_bookings.status as booking_status', 'as_employees.*', 'as_employees.status as employee_status','business_commissions.status as commission_status', DB::raw("CONCAT(varaa_consumers.first_name, ' ', varaa_consumers.last_name) as consumer_name")]);
 
