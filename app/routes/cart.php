af97bd617e7f1a6b8e699a0c07f3b117b09839ed
An Cao <kcj.pop.com@gmail.com>
2015-05-26 00:44:37 +0300

    Add filter to check cart existing.

--- app/routes/cart.php
+++ app/routes/cart.php
@@ -13,28 +13,30 @@ Route::group([
         'uses' => 'App\Cart\Controllers\Index@index'
     ]);
 
-    Route::get('checkout', [
-        'as' => 'cart.checkout',
-        'uses' => 'App\Cart\Controllers\Index@checkout'
-    ]);
+    Route::group(['before' => ['cart.existed']], function () {
+        Route::get('checkout', [
+            'as' => 'cart.checkout',
+            'uses' => 'App\Cart\Controllers\Index@checkout'
+        ]);
 
-    Route::get('consumer', [
-        'as' => 'cart.consumer',
-        'uses' => 'App\Cart\Controllers\Consumers@index'
-    ]);
+        Route::get('consumer', [
+            'as' => 'cart.consumer',
+            'uses' => 'App\Cart\Controllers\Consumers@index'
+        ]);
 
-    Route::post('consumer', [
-        'uses' => 'App\Cart\Controllers\Consumers@submit'
-    ]);
+        Route::post('consumer', [
+            'uses' => 'App\Cart\Controllers\Consumers@submit'
+        ]);
 
-    Route::get('remove/{id}', [
-        'as' => 'cart.remove',
-        'uses' => 'App\Cart\Controllers\Index@remove'
-    ]);
+        Route::get('remove/{id}', [
+            'as' => 'cart.remove',
+            'uses' => 'App\Cart\Controllers\Index@remove'
+        ]);
 
-    Route::post('payment', [
-        'as' => 'cart.payment',
-        'uses' => 'App\Cart\Controllers\Index@payment'
-    ]);
+        Route::post('payment', [
+            'as' => 'cart.payment',
+            'uses' => 'App\Cart\Controllers\Index@payment'
+        ]);
+    });
 
 });
