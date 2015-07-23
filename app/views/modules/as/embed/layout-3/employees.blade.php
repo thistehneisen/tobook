387972f0f66d789bd3de5d22b9945192a1c71e89
Hung Nguyen <hungnq1989@gmail.com>
2015-07-23 23:40:11 +0300

    Replace radio button with default button in CP #585

--- app/views/modules/as/embed/layout-3/employees.blade.php
+++ app/views/modules/as/embed/layout-3/employees.blade.php
@@ -1,13 +1,13 @@
 <div class="as-employee">
-    <label>
-        <input type="radio" name="employee_id" value="-1">
+    <label class="col-lg-12 col-sm-6 col-md-6 btn btn-default">
+        <input class="hidden" type="radio" name="employee_id" value="-1">
         {{ trans('common.anyone') }}
     </label>
 </div>
 @foreach ($employees as $employee)
 <div class="as-employee">
-    <label>
-        <input type="radio" name="employee_id" value="{{ $employee->id }}">
+    <label class="col-lg-12 col-sm-6 col-md-6 btn btn-default">
+        <input type="radio" class="hidden" name="employee_id" value="{{ $employee->id }}">
         {{ $employee->name }}
     </label>
 </div>
