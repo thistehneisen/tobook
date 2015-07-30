c4eb36226cacdf763d2e7697f3cab7e22e9844f9
Hung Nguyen <hungnq1989@gmail.com>
2015-07-30 16:59:51 +0300

    Add supported placeholders under message box

--- app/views/modules/as/options/page.blade.php
+++ app/views/modules/as/options/page.blade.php
@@ -25,7 +25,20 @@
         @foreach ($controls as $field)
         <div class="form-group">
             <label class="control-label col-sm-3">{{ trans('as.options.'.$page.'.'.$field->name) }}</label>
-            <div class="col-sm-6">{{ $field->render() }}</div>
+            <div class="col-sm-6">
+                {{ $field->render() }}
+                @if ($field->name === 'confirm_tokens_client' || $field->name === 'confirm_tokens_employee')
+                <br/>
+                <div class="alert alert-info">
+                    {Services}, {ServicesFull}, {Name}, {BookingID}, {Phone}, {Email}, {Notes}, {CancelURL}, {Address}, {Deposit}
+                </div>
+                @elseif ($field->name === 'confirm_consumer_sms_message' || $field->name === 'confirm_employee_sms_message')
+                <br/>
+                <div class="alert alert-info">
+                    {Services}, {ServicesFull}, {CancelURL}, {Address}, {Deposit}
+                </div>
+                @endif
+            </div>
         </div>
         @endforeach
     </div>
