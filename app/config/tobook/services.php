1289f9a682d780091dbab5e08b3c164509d67475
Hung Nguyen <hungnq1989@gmail.com>
2015-08-06 21:38:35 +0300

    Disable test for paygate in tobook

--- app/config/tobook/services.php
+++ app/config/tobook/services.php
@@ -4,7 +4,7 @@ return [
         'id'           => 65111,
         'password'     => '5f9ec9a8660a545bcba20ac5e2c2004a',
         'verification' => '7f478d2f4a92094f42b2047ed3a37c48',
-        'test'         => 1,
+        'test'         => 0,
     ],
 
     'oneapi' => [
