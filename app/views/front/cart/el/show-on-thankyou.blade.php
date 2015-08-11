ba3768e2d75d7c4c110bc52950c13c900ccba23d
Hung Nguyen <hungnq1989@gmail.com>
2015-08-11 17:58:25 +0300

    Fix syntax error and remove redundant css classes #605

--- app/views/front/cart/el/show-on-thankyou.blade.php
+++ app/views/front/cart/el/show-on-thankyou.blade.php
@@ -2,10 +2,10 @@
     <div class="col-sm-8 col-sm-offset-2">
         <table class="table table-striped">
             <tbody>
-                <tr class="cart-detail" id="cart-detail-30958">
+                <tr>
                     <td class="message">
-                        <h4 style="text-transform: uppercase">{{ trans('as.embed.thankyou_line1')) }}
-                        <h5>{{ trans('as.embed.thankyou_line2')) }}</h5>
+                        <h4 style="text-transform: uppercase">{{ trans('as.embed.thankyou_line1') }}
+                        <h5>{{ trans('as.embed.thankyou_line2') }}</h5>
                     </td>
                     <td class="icon">
                         <i class="fa fa-check-circle fa-3x"></i>
