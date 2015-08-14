deff55ad02629d0d6784ff3183bc35a4297d5db8
Antti Rössi <antti.rossi@varaa.com>
2015-08-14 10:46:32 +0300

    Add autoplay & remove suggested videos for Tobook tutorial video

--- app/config/tobook/varaa.php
+++ app/config/tobook/varaa.php
@@ -14,7 +14,7 @@ return [
     'languages'        => ['lv', 'ru'],
     'default_language' => 'lv',
     'default_coords'   => [56.9462031, 24.1042872], // Riga,
-    'tutorial_video'   => 'https://www.youtube.com/watch?v=FrMUPi7Yo7U',
+    'tutorial_video'   => 'https://www.youtube.com/watch?v=FrMUPi7Yo7U?rel=0&amp;showinfo=0&autoplay=1',
     'premium_modules'  => [
         'appointment' => [
             'route_name' => 'as.index',
