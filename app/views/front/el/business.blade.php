9fd15b402d26f4ae868c6b13e2e6a11b79cfb219
An Cao <kcj.pop.com@gmail.com>
2015-08-21 01:29:36 +0300

    Init the layout.

--- app/views/front/el/business.blade.php
+++ app/views/front/el/business.blade.php
@@ -27,6 +27,167 @@
         <div class="description">
             {{ $business->description_html }}
         </div>
+
+        <h3 class="sub-heading">Book your service online</h3>
+        <div class="cp-booking-form">
+            <div class="content">
+                <div class="panel-group" id="js-cp-booking-form-categories" role="tablist">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#js-cp-booking-form-categories" href="#js-category-1">
+                                    Category name
+                                    <span class="pull-right">9 services</span>
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="js-category-1" class="panel-collapse collapse in" role="tabpanel">
+                            <div class="panel-body">
+                                <div class="panel-group panel-group-service" id="js-cp-booking-form-categories-1" role="tablist">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading" role="tab">
+                                            <h4 class="panel-title">
+                                                <a role="button" data-toggle="collapse" data-parent="#js-cp-booking-form-categories-1" href="#js-service-1">
+                                                    Service with custom time
+                                                </a>
+                                            </h4>
+                                        </div>
+                                        <div id="js-service-1" class="panel-collapse collapse in" role="tabpanel">
+                                            <div class="panel-body">
+                                                <div class="service">
+                                                    <div class="row">
+                                                        <div class="col-md-10">
+                                                            <div class="service-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aliquid fugit beatae labore provident unde, quasi, tempore, reiciendis amet inventore, delectus recusandae et dolorem ut maxime autem obcaecati pariatur corporis.</div>
+                                                            <p>60min/ 70&euro;</p>
+                                                        </div>
+                                                        <div class="col-md-2">
+                                                            <button class="btn btn-orange pull-right">Select</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="service">
+                                                    <div class="row">
+                                                        <div class="col-md-10">
+                                                            <div class="service-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aliquid fugit beatae labore provident unde, quasi, tempore, reiciendis amet inventore, delectus recusandae et dolorem ut maxime autem obcaecati pariatur corporis.</div>
+                                                            <p>60min/ 70&euro;</p>
+                                                        </div>
+                                                        <div class="col-md-2">
+                                                            <button class="btn btn-orange pull-right">Select</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel-group-service">
+                                    <div class="single-service">
+                                        <h4 class="panel-title">Service name</h4>
+                                        <div class="row">
+                                            <div class="col-md-10">
+                                                <div class="service-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aliquid fugit beatae labore provident unde, quasi, tempore, reiciendis amet inventore, delectus recusandae et dolorem ut maxime autem obcaecati pariatur corporis.</div>
+                                                <p>60min/ 70&euro;</p>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <button class="btn btn-orange pull-right">Select</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#js-cp-booking-form-categories" href="#js-category-2">
+                                    Category 2
+                                    <span class="pull-right">9 services</span>
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="js-category-2" class="panel-collapse collapse" role="tabpanel">
+                            <div class="panel-body">
+                                <div class="panel-group-service">
+                                    <div class="single-service">
+                                        <h4 class="panel-title">Service name</h4>
+                                        <div class="row">
+                                            <div class="col-md-10">
+                                                <div class="service-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aliquid fugit beatae labore provident unde, quasi, tempore, reiciendis amet inventore, delectus recusandae et dolorem ut maxime autem obcaecati pariatur corporis.</div>
+                                                <p>60min/ 70&euro;</p>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <button class="btn btn-orange pull-right">Select</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="navigation">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="#" class="btn btn-orange hidden">Go back</a>
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        <a href="#" class="btn btn-orange pull-right">Continue</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="cp-booking-form">
+            <div class="content">
+                <div class="row">
+                    <div class="col-sm-4"><strong>Choose the employee</strong></div>
+                    <div class="col-sm-2">Employee1</div>
+                    <div class="col-sm-2">Employee2</div>
+                    <div class="col-sm-2">Employee3</div>
+                    <div class="col-sm-2">Employee4</div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-1"><i class="glyphicon glyphicon-chevron-left"></i></div>
+                    <div class="col-sm-10">
+                        <ul class="dates">
+                            <li>17.8</li>
+                            <li>17.8</li>
+                            <li>17.8</li>
+                            <li>17.8</li>
+                            <li>17.8</li>
+                            <li>17.8</li>
+                            <li>17.8</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-1 text-right"><i class="glyphicon glyphicon-chevron-right"></i></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-offset-1 col-sm-10">
+                        <ul class="time-options">
+                            <li>08:00</li>
+                            <li>09:00</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="navigation">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="#" class="btn btn-orange">Go back</a>
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        <a href="#" class="btn btn-orange pull-right">Continue</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     {{-- right sidebar --}}
