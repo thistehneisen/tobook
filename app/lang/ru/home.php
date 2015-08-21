092fd056706a5b3ac7b7ef99535a332defb81ad1
Hung Nguyen <hungnq1989@gmail.com>
2015-08-21 23:57:27 +0300

    Update tutorial image link for RU

--- app/lang/ru/home.php
+++ app/lang/ru/home.php
@@ -60,7 +60,7 @@ return [
     'search_place'          => 'Helsinki, FI',
     'video_tutorial_text'   => 'Как это работает?',
     'video_tutorial_link'   => 'https://www.youtube.com/watch?v=0G8U4xZCgLY?rel=0&amp;showinfo=0&autoplay=1',
-    'image_tutorial_link'   => asset_path('core/img/how-does-it-work.png'),
+    'image_tutorial_link'   => asset_path('core/img/how-does-it-work-ru.png'),
     'search'        => [
         'tagline'         => 'Что вы хотите заказать?',
         'query'           => 'Введите компанию',
