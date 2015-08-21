ceb6358bd3b0f0aeeac7d33d0ddfc6e5c667a024
Hung Nguyen <hungnq1989@gmail.com>
2015-08-21 23:25:54 +0300

    Update length counter for name text box in sms template #616

--- resources/varaa/core/scripts/sms-length.js
+++ resources/varaa/core/scripts/sms-length.js
@@ -2,12 +2,13 @@ $(function () {
     var charset7bit = ['@', '£', '$', '¥', 'è', 'é', 'ù', 'ì', 'ò', 'Ç', "\n", 'Ø', 'ø', "\r", 'Å', 'å', 'Δ', '_', 'Φ', 'Γ', 'Λ', 'Ω', 'Π', 'Ψ', 'Σ', 'Θ', 'Ξ', 'Æ', 'æ', 'ß', 'É', ' ', '!', '"', '#', '¤', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '¡', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ä', 'Ö', 'Ñ', 'Ü', '§', '¿', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ñ', 'ü', 'à'];
     var charset7bitext = ["\f", '^', '{', '}', '\\', '[', '~', ']', '|', '€'];
 
-    function updateSmsLength() {
-        var content = $('#content').val();
+    function updateSmsLength(id, maxlength1, maxlength2) {
+        var target = $('#'+id);
+        var content = target.val();
         var chars_arr = content.split("");
         var coding = '7bit';
         var chars_used = 0;
-        var chars_sms = 160;
+        var chars_sms = maxlength1;
         for (i = 0; i < chars_arr.length; i++) {
             if (charset7bit.indexOf(chars_arr[i]) >= 0) {
                 chars_used = chars_used + 1;
@@ -21,60 +22,62 @@ $(function () {
         }
 
         if (coding == '7bit') {
-            if (chars_used > 160) {
-                chars_sms = 153;
+            if (chars_used > maxlength1) {
+                chars_sms = maxlength1 - 3;
             } else {
-                chars_sms = 160;
+                chars_sms = maxlength1;
             }
         } else {
-            if (chars_used > 70) {
-                chars_sms = 67;
+            if (chars_used > maxlength2) {
+                chars_sms = maxlength2 - 3;
             } else {
-                chars_sms = 70;
+                chars_sms = maxlength2;
             }
         }
 
-        $('#content').attr('maxlength', chars_sms);
+        target.attr('maxlength', chars_sms);
 
         var msg = chars_used + '/' + chars_sms;
-        if($('#label-max-length').length == 0) {
+        if($('#label-max-length-'+id).length == 0) {
             $('<span>', {
                 'text': msg,
                 'class': 'label label-success',
-                'id' : 'label-max-length'
+                'id' : 'label-max-length-'+id
             }).appendTo($('#lomake-form'));
-             $('#label-max-length').css({
+             $('#label-max-length-'+id).css({
                 'display' : 'block',
                 'position': 'absolute',
                 'white-space':  'nowrap',
                 'z-index' : '1099'
              });
-            $('#label-max-length').css('top', $('#content').offset().top + $('#content').height() + 12);
-            $('#label-max-length').css('left', $('#content').offset().left + ($('#content').width() / 2) - 12);
+            $('#label-max-length-'+id).css('top', target.offset().top + target.height() + 12);
+            $('#label-max-length-'+id).css('left', target.offset().left + (target.width() / 2) - 12);
         } else {
-            $('#label-max-length').html(msg);
+            $('#label-max-length-'+id).html(msg);
         }
 
         if(chars_used >= chars_sms) {
-            $('#label-max-length').removeClass(('label-success')).addClass('label-danger');
+            $('#label-max-length-'+id).removeClass(('label-success')).addClass('label-danger');
             var content = content.split('').splice(0, chars_sms);
-            $('#content').val(content.join(''));
+            target.val(content.join(''));
         } else {
-            $('#label-max-length').removeClass(('label-danger')).addClass('label-success');
+            $('#label-max-length-'+id).removeClass(('label-danger')).addClass('label-success');
         }
     }
 
-    var updateTimer = null;
+    function setUp(id, maxlength1, maxlength2) {
+        $('#'+id).keyup(function(ev) {
+            updateSmsLength(id, maxlength1, maxlength2);
+        });
 
-    $('#content').keyup(function(ev) {
-        updateSmsLength();
-    });
+        $('#'+id).blur(function(e){
+            $('#label-max-length-'+id).hide();
+        });
 
-    $('#content').blur(function(e){
-        $('#label-max-length').hide();
-    });
-
-    $('#content').focus(function(e){
-        $('#label-max-length').show();
-    });
+        $('#'+id).focus(function(e){
+            $('#label-max-length-'+id).show();
+        });
+    }
+    setUp('content', 160, 70);
+    setUp('from_name', 10, 10);
 });
