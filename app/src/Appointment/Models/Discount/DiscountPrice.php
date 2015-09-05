8dba7732cabcfe827ac0eaf93dd7194db36088f5
Hung Nguyen <hungnq1989@gmail.com>
2015-09-05 15:42:49 +0300

    Make a separated trait for re-using getDiscountPrice method #619 #631

---
+++ app/src/Appointment/Models/Discount/DiscountPrice.php
@@ -0,0 +1,57 @@
+<?php namespace App\Appointment\Models\Discount;
+use App\Appointment\Models\Discount;
+use App\Appointment\Models\DiscountLastMinute;
+use Carbon\Carbon;
+
+trait DiscountPrice {
+
+    /**
+     * Calculate discount price based on booking time
+     *
+     * @date booking date
+     * @time booking time
+     * @return double price
+     */
+    public function getDiscountPrice($date, $time)
+    {
+        $startTime = ($time instanceof Carbon)
+            ? $time
+            : Carbon::createFromFormat('Y-m-d H:i:s', sprintf('%s %s:00', $date->toDateString(), $time));
+
+        $weekdays  = [
+            Carbon::MONDAY    => 'mon',
+            Carbon::TUESDAY   => 'tue',
+            Carbon::WEDNESDAY => 'wed',
+            Carbon::THURSDAY  => 'thu',
+            Carbon::FRIDAY    => 'fri',
+            Carbon::SATURDAY  => 'sat',
+            Carbon::SUNDAY    => 'sun'
+        ];
+        $weekday   = $weekdays[$startTime->dayOfWeek];
+
+        $now       = Carbon::now();
+        $formatted = sprintf('%02d:%02d:00', $startTime->hour, $startTime->minute);
+        $discount = Discount::where('user_id', '=', $this->user->id)
+            ->where(function($query) use($weekday, $formatted) {
+                $query->where('start_at', '<=', $formatted)
+                ->where('end_at', '>=', $formatted);
+            })->where('weekday', '=', $weekday)->first();
+
+        $discountLastMinute = DiscountLastMinute::find($this->user->id);
+        $price = $this->price;
+
+        if (!empty($discount)) {
+            $price = (double) $this->price * (1 - ((double) $discount->discount / 100));
+        }
+
+        if (!empty($discountLastMinute)) {
+            if($discountLastMinute->is_active
+                && $now->diffInHours($startTime) <= $discountLastMinute->before) {
+                $price = (double)  $this->price * (1 - ((double) $discountLastMinute->discount / 100));
+            }
+        }
+
+        return $price;
+    }
+
+}
--- app/src/Appointment/Models/Service.php
+++ app/src/Appointment/Models/Service.php
@@ -21,6 +21,8 @@ class Service extends \App\Core\Models\Base
         ]
     ];
 
+    use \App\Appointment\Models\Discount\DiscountPrice;
+
     /**
      * @see \App\Core\Models\Base
      */
@@ -182,48 +184,6 @@ class Service extends \App\Core\Models\Base
             'App\Appointment\Models\Room',
             'as_room_service'
         );
-    }
-
-    public function getDiscountPrice($date, $time)
-    {
-        $startTime = ($time instanceof Carbon)
-            ? $time
-            : Carbon::createFromFormat('Y-m-d H:i:s', sprintf('%s %s:00', $date->toDateString(), $time));
-
-        $weekdays  = [
-            Carbon::MONDAY    => 'mon',
-            Carbon::TUESDAY   => 'tue',
-            Carbon::WEDNESDAY => 'wed',
-            Carbon::THURSDAY  => 'thu',
-            Carbon::FRIDAY    => 'fri',
-            Carbon::SATURDAY  => 'sat',
-            Carbon::SUNDAY    => 'sun'
-        ];
-        $weekday   = $weekdays[$startTime->dayOfWeek];
-
-        $now       = Carbon::now();
-        $formatted = sprintf('%02d:%02d:00', $startTime->hour, $startTime->minute);
-        $discount = Discount::where('user_id', '=', $this->user->id)
-            ->where(function($query) use($weekday, $formatted) {
-                $query->where('start_at', '<=', $formatted)
-                ->where('end_at', '>=', $formatted);
-            })->where('weekday', '=', $weekday)->first();
-
-        $discountLastMinute = DiscountLastMinute::find($this->user->id);
-        $price = $this->price;
-
-        if (!empty($discount)) {
-            $price = (double) $this->price * (1 - ((double) $discount->discount / 100));
-        }
-
-        if (!empty($discountLastMinute)) {
-            if($discountLastMinute->is_active
-                && $now->diffInHours($startTime) <= $discountLastMinute->before) {
-                $price = (double)  $this->price * (1 - ((double) $discountLastMinute->discount / 100));
-            }
-        }
-
-        return $price;
     }
 
     /**
