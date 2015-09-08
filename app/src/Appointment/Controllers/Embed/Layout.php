69f8f2081a962e15d5ec3e339bccf713473eb0b4
Hung Nguyen <hungnq1989@gmail.com>
2015-09-08 14:39:07 +0300

    Only fetch discount data for CP layout #641

--- app/src/Appointment/Controllers/Embed/Layout.php
+++ app/src/Appointment/Controllers/Embed/Layout.php
@@ -115,20 +115,12 @@ trait Layout
             ->where('is_show_front', true)
             ->get();
 
-        $priceRange  = [];
-        $hasDiscount = [];
-        $servicesDiscount = [];
-        foreach ($categories as $category) {
-            $priceRange[$category->id]  = $category->priceRange;
-            $hasDiscount[$category->id] = $category->hasDiscount;
-            foreach ($category->services as $asService) {
-                $servicesDiscount[$asService->id] = ($hasDiscount[$category->id] && $asService->is_discount_included);
-            }
-        }
-
         $layout = empty($layout)
             ? $this->getLayout()
             : $layout;
+
+        list($priceRange, $hasDiscount, $servicesDiscount) = $this->getDiscountData($categories, $layout);
+
 
         $minDistance = ((int) $user->asOptions['min_distance'])
             ? sprintf('+%dd', (int) $user->asOptions['min_distance'])
@@ -162,6 +154,35 @@ trait Layout
             'maxDistance'        => $maxDistance,
             'action'             => $action
         ];
+    }
+
+    /**
+     * Fetch discount data for displaying on CP layout
+     *
+     * @param Illuminate\Support\Collection $categories
+     * @param strinng $layout
+     *
+     * @return Array
+     */
+    public function getDiscountData($categories, $layout)
+    {
+        $priceRange       = [];
+        $hasDiscount      = [];
+        $servicesDiscount = [];
+
+        if ($layout === 'cp') {
+            return [$priceRange, $hasDiscount, $servicesDiscount];
+        }
+
+        foreach ($categories as $category) {
+            $priceRange[$category->id]  = $category->priceRange;
+            $hasDiscount[$category->id] = $category->hasDiscount;
+            foreach ($category->services as $service) {
+                $servicesDiscount[$service->id] = ($hasDiscount[$category->id] && $service->is_discount_included);
+            }
+        }
+
+        return [$priceRange, $hasDiscount, $servicesDiscount];
     }
 
     /**
