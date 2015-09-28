52846fb82fe5f753cd0a5e27b8389df29215e339
Nguyen Hung <hungnq@Nguyens-MBP.home>
2015-09-28 18:26:36 +0300

    Add Statistics model to generate Report :construction_worker: #670

--- app/src/Core/Controllers/Admin/Statistics.php
+++ app/src/Core/Controllers/Admin/Statistics.php
@@ -1,11 +1,11 @@
 <?php namespace App\Core\Controllers\Admin;
 
 use App;
-use App\Core\Models\Setting;
-use App\Lomake\FieldFactory;
+use App\Core\Models\Report\Statistics as StatisticsModel;
 use Config;
 use Input;
 use Redirect;
+use Carbon\Carbon;
 
 class Statistics extends Base
 {
@@ -14,8 +14,18 @@ class Statistics extends Base
 	public function index()
 	{
 
+		$today = Carbon::now();
+        $start = Input::has('start')
+            ? carbon_date(Input::get('start'))
+            : $today->copy()->subMonth();
+        $end = Input::has('end')
+            ? carbon_date(Input::get('end'))
+            : $today;
+     
         return $this->render('index', [
-
+            'start'  => str_date($start),
+            'end'    => str_date($end),
+            'report' =>  new StatisticsModel($start, $end),
         ]);
 	}
 }
