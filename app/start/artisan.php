b04d3752fdb9c9c53f7e997328c09d2284d38fcf
Q.Hung <hung@varaa.com>
2015-10-12 16:51:30 +0300

    Remove duplicate workshift entries in db, keep latest one :beetle: :hocho: #691

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -32,6 +32,7 @@ Artisan::add(new App\Core\Commands\FixPayAtVenueCommisions());
 Artisan::add(new App\Core\Commands\FixMissingCommissionsToBook());
 Artisan::add(new App\Core\Commands\FixMissingNewConsumerCommissions());
 Artisan::add(new App\Core\Commands\UpdateMinDistanceHourUnit());
+Artisan::add(new App\Core\Commands\FixWorkshiftDuplicates());
 
 Artisan::add(new App\Cart\Commands\UnlockCartItemsCommand());
 
