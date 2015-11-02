ec6d16a06d97ebf2d1d601955c156ca78cf8a632
Q.Hung <hungnq1989@gmail.com>
2015-11-02 19:33:43 +0200

    Fix bug data missing when expire at is empty #705

--- app/src/Core/Controllers/Admin/Coupon.php
+++ app/src/Core/Controllers/Admin/Coupon.php
@@ -156,9 +156,15 @@ class Coupon extends Base
 
         try {
             $data = Input::all();
-            $data['begin_at']  = carbon_date(Input::get('begin_at'));
-            $data['expire_at'] = carbon_date(Input::get('expire_at'));
+
+            if (!empty(Input::get('begin_at'))) {
+                $data['begin_at']  = carbon_date(Input::get('begin_at'));
+            }
             
+            if (!empty(Input::get('expire_at'))) {
+                $data['expire_at'] = carbon_date(Input::get('expire_at'));
+            }
+
             $campaign->fill($data);
             $campaign->saveOrFail();
 
@@ -189,7 +195,7 @@ class Coupon extends Base
         } catch(\Watson\Validating\ValidationException $ex){
             return Redirect::back()->withInput()->withErrors($ex->getErrors());
         }
-        
+
         return Redirect::route('admin.coupon.campaigns');
     }
 }
