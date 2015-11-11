f7d86147a27a557a59e77a4d15f2cd5efe47921a
Q.Hung <hungnq1989@gmail.com>
2015-11-11 16:56:39 +0200

    Commmand generate deployment info in public/rev.txt #704

--- app/src/Core/Commands/GenerateDeploymentInfo.php
+++ app/src/Core/Commands/GenerateDeploymentInfo.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Carbon\Carbon;
 
 class GenerateDeploymentInfo extends Command {
 
@@ -39,7 +40,33 @@ class GenerateDeploymentInfo extends Command {
 	 */
 	public function fire()
 	{
-		$process = new Process('git symbolic-ref --short -q HEAD');
+		$template = "Date: {date}
+Entity: {entity}
+Branch/tag: {branch}
+Commit: {commit}
+Version:{version}
+";
+		$now    = Carbon::now();
+		$date   = $now->format('l jS F Y h:i:s A');
+		$branch = $this->exec('git symbolic-ref --short -q HEAD');
+		$entity = $this->exec('php artisan env');
+		$commit = $this->exec("git log --pretty=format:\'%h\' -n 1");
+		$version = $this->exec('git describe');
+
+		$entity = str_replace('Current application environment: ', '', $entity);
+
+		$template = str_replace('{date}', trim($date), $template);
+		$template = str_replace('{entity}', trim($entity), $template);
+		$template = str_replace('{branch}', trim($branch), $template);
+		$template = str_replace('{commit}', trim($commit), $template);
+		$template = str_replace('{version}', trim($version), $template);
+		//$this->info($template);
+		file_put_contents('./public/rev.txt', $template);
+	}
+
+	public function exec($command)
+	{
+		$process = new Process($command);
 		$process->run();
 		
 		// executes after the command finishes
@@ -47,7 +74,7 @@ class GenerateDeploymentInfo extends Command {
 		    throw new ProcessFailedException($process);
 		}
 
-		$this->info($process->getOutput());
+		return $process->getOutput();
 	}
 
 }
