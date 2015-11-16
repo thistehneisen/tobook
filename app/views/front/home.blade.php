698188a230be0f59822dad4aa6e7fab74d447089
Q.Hung <hungnq1989@gmail.com>
2015-11-16 15:56:50 +0200

    Don't show green ribbon when discount = 0 #718

--- app/views/front/home.blade.php
+++ app/views/front/home.blade.php
@@ -71,7 +71,7 @@
     @foreach ($randomBusinesses as $business)
         <div class="col-sm-3">
             <div class="discount-widget-containter">
-                <div class="ribbon-wrapper-green"><div class="ribbon-green">{{ $business->discountPercent }}% OFF</div></div>
+                @if($business->discountPercent> 0)<div class="ribbon-wrapper-green"><div class="ribbon-green">{{ $business->discountPercent }}% OFF</div></div>@endif
                 <a href="{{ route('business.index', ['id' => $business->user_id, 'slug' => $business->slug]) }}" class="offer-title action-uri"><img alt="100%x180" style="height: 180px; width: 100%; display: block;" src="{{ $business->image }}" alt="{{ $business->name }}"></a>
                 <div class="discount-service-rating">
                     <span class="raty" data-score="5"></span><span class="pull-right">{{ $business->reviewCount }} {{ trans('common.review') }}</span>
