f71974172f1b04d621c863dc0f8fd0aad4ed3e61
Q.Hung <hungnq1989@gmail.com>
2015-11-25 17:50:54 +0200

    Cast integer to boolean

--- app/src/Consumers/Models/EmailTemplate.php
+++ app/src/Consumers/Models/EmailTemplate.php
@@ -61,6 +61,7 @@ class EmailTemplate extends \App\Core\Models\Base
 
         foreach ($consumers as $consumer) {
             if (empty($consumer->email) || !(bool)$consumer->receive_email) {
+                Log::info('Skipp email', [$consumer->email]);
                 continue;
             }
             
