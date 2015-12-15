d71e239a670446086c5df7d9a84410724b70d6e8
Q.Hung <hungnq1989@gmail.com>
2015-12-15 22:54:48 +0200

    Temporarily remove custom time validation when booking is place #740

--- app/src/Appointment/Models/Reception/FrontendReceptionist.php
+++ app/src/Appointment/Models/Reception/FrontendReceptionist.php
@@ -41,7 +41,7 @@ class FrontendReceptionist extends Receptionist
     public function validateBooking()
     {
         $this->validateWithEmployeeFreetime();
-        $this->isValidWithCustomTime();
+        // $this->isValidWithCustomTime();
         $this->validateWithExistingBooking();
         $this->validateWithResources();
         $this->validateWithRooms();
