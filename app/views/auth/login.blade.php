f76e5603f6ec2915d0dfae641b7f700ee07bd6d9
Q.Hung <hungnq1989@gmail.com>
2016-01-27 16:37:22 +0200

    Remove comfortaa font for login button #787

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -14,7 +14,7 @@
 <div class="row">
     <div class="col-xs-12">
         <h1 class="comfortaa orange text-center">{{ trans('common.sign_in_header') }}</h1>
-        <h4 class="comfortaa text-center">{{ trans('user.fill_fields') }}</h4>
+        <h4 class="text-center">{{ trans('user.fill_fields') }}</h4>
 
         @include ('el.messages')
 
@@ -41,7 +41,7 @@
 
             <div class="form-group">
                 <div class="col-sm-9 text-right">
-                    <button type="submit" id="btn-login" class="btn btn-lg btn-orange to-upper comfortaa">{{ trans('common.sign_in') }}</button>
+                    <button type="submit" id="btn-login" class="btn btn-lg btn-orange to-upper">{{ trans('common.sign_in') }}</button>
                 </div>
             </div>
         {{ Form::close() }}
